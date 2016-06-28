329ff476d8d3baca98a91ea0b9fa5288478bac6a
Yu Shu <ys2530@nyu.edu>
2016-06-28 16:17:07 -0400

    [219. Contains Duplicate II][Time Limit Exceeded]committed by Anthony

---
+++ 219-Contains-Duplicate-II/solution.java
@@ -0,0 +1,24 @@
+/**
+ * Contains Duplicate II
+ * 
+ */ 
+
+
+// s1: brute force
+// O(n^2), O(1)
+
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return false;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && j - i <= k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
