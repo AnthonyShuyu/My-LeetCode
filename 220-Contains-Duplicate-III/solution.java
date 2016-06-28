a31ebeca5d258a5d283cce296ce3b319b666c6b5
Yu Shu <ys2530@nyu.edu>
2016-06-28 18:37:38 -0400

    [220. Contains Duplicate III][Compile Error]committed by Anthony

---
+++ 220-Contains-Duplicate-III/solution.java
@@ -0,0 +1,24 @@
+/**
+ * Contains Duplicate III
+ * 
+ */
+
+
+// s1: brute force, 2 for loops
+// O(nk), O(1)
+
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (nums == null || nums.length == 0 || k <= 0 || t <= 0) {
+            return false;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j <= i + k && j < nums.length; j++) {
+                if (Math.abs(nums[i],nums[j]) <= t) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
