c3dcd59653e2bea2c5ca69ce1c41331fd1d26446
Yu Shu <ys2530@nyu.edu>
2016-06-28 18:48:35 -0400

    [220. Contains Duplicate III][Time Limit Exceeded]committed by Anthony

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -9,13 +9,15 @@
 
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if (nums == null || nums.length == 0 || k <= 0 || t <= 0) {
+        if (nums == null || nums.length == 0 || k <= 0 || t < 0) {
             return false;
         }
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j <= i + k && j < nums.length; j++) {
-                if (Math.abs(nums[i],nums[j]) <= t) {
-                    return true;
+                if (Math.abs((long)nums[i] - (long)nums[j]) <= Integer.MAX_VALUE) {
+                    if (Math.abs(nums[i] - nums[j]) <= t) {
+                        return true;
+                 }
                 }
             }
         }
