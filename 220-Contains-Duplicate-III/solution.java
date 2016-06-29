c3e8106bd6cb8dcb87c7dcd897aadcd3c2681a93
Yu Shu <ys2530@nyu.edu>
2016-06-28 20:43:38 -0400

    [220. Contains Duplicate III][Compile Error]committed by Anthony

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -33,6 +33,7 @@ public class Solution {
 // s2: use 
 // O(n), O(n)
 
+/*
 public class Solution{
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         if (nums == null || nums.length == 0 || k <= 0 || t < 0) {
@@ -47,7 +48,7 @@ public class Solution{
             
             
         // }
-        for (int i = 0; i < num.length - k; i++) {
+        for (int i = 0; i < nums.length - k; i++) {
             if (checkT(i, i + k, nums, t)) {
                 return true;
             }
@@ -65,6 +66,30 @@ public class Solution{
         }
         return false;
     }
+    
+*/
+
+
+ public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (k < 1 || t < 0) return false;
+        Map<Long, Long> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            long remappedNum = (long) nums[i] - Integer.MIN_VALUE;
+            long bucket = remappedNum / ((long) t + 1);
+            if (map.containsKey(bucket)
+                    || (map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= t)
+                        || (map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= t))
+                            return true;
+            if (map.entrySet().size() >= k) {
+                long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
+                map.remove(lastBucket);
+            }
+            map.put(bucket, remappedNum);
+        }
+        return false;
+    }
+}
     
     
 }
