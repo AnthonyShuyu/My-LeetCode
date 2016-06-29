6e0e3dbbb9938f9f3ccd033bd4d13b1a41cf39ce
Yu Shu <ys2530@nyu.edu>
2016-06-28 20:23:59 -0400

    [220. Contains Duplicate III][Compile Error]committed by Anthony

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -6,7 +6,9 @@
 
 // s1: brute force, 2 for loops
 // O(nk), O(1)
+// Time limit exceeded
 
+/*
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         if (nums == null || nums.length == 0 || k <= 0 || t < 0) {
@@ -23,4 +25,46 @@ public class Solution {
         }
         return false;
     }
+}
+*/
+
+
+
+// s2: use 
+// O(n), O(n)
+
+public class Solution{
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (nums == null || nums.length == 0 || k <= 0 || t < 0) {
+            return false;
+        }
+        
+        // Set<Integer> hashSet = new HashSet<Integer>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (i > k) {
+        //         hashSet.remove(nums[i - k - 1]);
+        //     } 
+            
+            
+        // }
+        for (int i = 0; i < num.length - k; i++) {
+            if (checkT(i, i + k, nums, t)) {
+                return true;
+            }
+        }
+        return false;        
+    }
+    
+    public boolean checkT(int start, int end, int[] nums, int t) {
+        for (int i = start; i <= end; i++) {
+            for (int j = i + 1; j <= end; j++) {
+                if (Math.abs(nums[i] - nums[j]) <= t) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    
 }
