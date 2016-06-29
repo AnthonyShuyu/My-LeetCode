9bb3e1a56f48b385a8a6059040e00a2770ec303d
Yu Shu <ys2530@nyu.edu>
2016-06-28 20:57:56 -0400

    [220. Contains Duplicate III][Accepted]committed by Anthony

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -70,6 +70,7 @@ public class Solution{
 */
 
 
+/*
  public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         if (k < 1 || t < 0) return false;
@@ -90,5 +91,31 @@ public class Solution{
         return false;
     }
 }
+*/
     
-    
+
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return false;
+        }
+
+        final TreeSet<Integer> values = new TreeSet<>();
+        for (int ind = 0; ind < nums.length; ind++) {
+
+            final Integer floor = values.floor(nums[ind] + t);
+            final Integer ceil = values.ceiling(nums[ind] - t);
+            if ((floor != null && floor >= nums[ind])
+                    || (ceil != null && ceil <= nums[ind])) {
+                return true;
+            }
+
+            values.add(nums[ind]);
+            if (ind >= k) {
+                values.remove(nums[ind - k]);
+            }
+        }
+
+        return false;
+    }
+}
