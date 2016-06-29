35591f9b7b65cc1aa331ad9d9850747375de62f7
Yu Shu <ys2530@nyu.edu>
2016-06-29 00:37:52 -0400

    [344. Reverse String][Accepted]committed by Anthony

--- 344-Reverse-String/solution.java
+++ 344-Reverse-String/solution.java
@@ -19,7 +19,7 @@ public class Solution {
     public char[] reverseChars(char[] chars) {
         for (int i = 0, j = chars.length - 1; i < j; i++, j--) {
             char temp = chars[i];
-            char[i] = chars[j];
+            chars[i] = chars[j];
             chars[j] = temp;
         }
         return chars;
