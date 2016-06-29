c18a83e42fad1d29d730f3dd5150019a20edcf05
Yu Shu <ys2530@nyu.edu>
2016-06-29 00:37:24 -0400

    [344. Reverse String][Compile Error]committed by Anthony

---
+++ 344-Reverse-String/solution.java
@@ -0,0 +1,27 @@
+/**
+ * reverse String
+ * 
+ */
+
+
+// s1: String.toCharArray()
+// 
+
+public class Solution {
+    public String reverseString(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        char[] chars = s.toCharArray();
+        return String.valueOf(reverseChars(chars));
+    }
+    
+    public char[] reverseChars(char[] chars) {
+        for (int i = 0, j = chars.length - 1; i < j; i++, j--) {
+            char temp = chars[i];
+            char[i] = chars[j];
+            chars[j] = temp;
+        }
+        return chars;
+    }
+}
