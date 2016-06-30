bd605f9e85c1b3498cb739087842da72d373ecc4
Yu Shu <ys2530@nyu.edu>
2016-06-29 20:22:56 -0400

    [220. Contains Duplicate III][Accepted]committed by Anthony

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -60,8 +60,14 @@ public class Solution{
 
 
 // s3: Balanced Search Tree, use TreeSet
-// 
+// O(NlogK), O(n)
 
+
+/*
+This problem requires to maintain a window of size k of the previous values that can be queried for value ranges. The best data structure to do that is Binary Search Tree. As a result maintaining the tree of size k will result in time complexity O(N lg K). In order to check if there exists any value of range abs(nums[i] - nums[j]) to simple queries can be executed both of time complexity O(lg K)
+
+Here is the whole solution using TreeMap.
+*/
 
 public class Solution{
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
@@ -84,40 +90,3 @@ public class Solution{
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-public class Solution {
-    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if (nums == null || nums.length == 0 || k <= 0 || t < 0) {
-            return false;
-        }
-
-        TreeSet<Integer> values = new TreeSet<Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            Integer floor = values.floor(nums[i] + t);
-            Integer ceil = values.ceiling(nums[i] - t);
-            if ((floor != null && floor >= nums[i])
-                    || (ceil != null && ceil <= nums[i])) {
-                return true;
-            }
-
-            values.add(nums[i]);
-            if (i >= k) {
-                values.remove(nums[i - k]);
-            }
-        }
-        return false;
-    }
-}
-*/
