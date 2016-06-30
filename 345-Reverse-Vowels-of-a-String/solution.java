c765a43035eaf8ddd7899e47f3d51fe79a4cf522
Yu Shu <ys2530@nyu.edu>
2016-06-29 20:41:45 -0400

    [345. Reverse Vowels of a String][Compile Error]committed by Anthony

---
+++ 345-Reverse-Vowels-of-a-String/solution.java
@@ -0,0 +1,47 @@
+/* 
+* Reverse Vowels of a String
+* A, E, I, O, U are 5 vowels in a String
+* Ask questions: What are the characters in the String?
+* Assume: characters in the String are all lower case letters
+*
+*/
+
+// 
+
+public class Solution {
+    public String reverseVowels(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        char[] charArray = s.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+        while (start < end) {
+            while (!isVowel(charArray[start])) {
+                start++;
+            }
+            while (!isVowel(charArray[end])) {
+                end--;
+            }
+            swap(charArray, start, end);
+            start++;
+            end--;
+        }
+    }
+    
+    public void swap(char[] charArray, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        char temp = charArray[start];
+        charArray[start] = charArray[end];
+        charArray[end] = temp;
+    }
+    
+    public boolean isVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            return true;
+        }
+        return false;
+    }
+}
