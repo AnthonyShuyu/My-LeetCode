ab1a89381224fdecc2e3133a61193922686c2149
Yu Shu <ys2530@nyu.edu>
2016-06-30 13:37:01 -0400

    [287. Find the Duplicate Number][Accepted]committed by Anthony

---
+++ 287-Find-the-Duplicate-Number/solution.java
@@ -0,0 +1,24 @@
+/**
+ * Find the Duplicate Number
+ * 
+ */ 
+
+
+// s1: brute force
+// O(n^2), O(1)
+
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return 0;
+    }
+}
