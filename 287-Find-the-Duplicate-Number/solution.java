dcc84bfed9c8025ff1b373292ff7945d6e34edcf
Yu Shu <ys2530@nyu.edu>
2016-06-30 14:40:46 -0400

    [287. Find the Duplicate Number][Accepted]committed by Anthony

--- 287-Find-the-Duplicate-Number/solution.java
+++ 287-Find-the-Duplicate-Number/solution.java
@@ -121,7 +121,7 @@ public class Solution{
 */
 
 
-// s6: two pointers, slow and faster pointers
+// *** s6: two pointers, slow and faster pointers
 // O(n), O(1)
 // not modify the original array
 
@@ -144,3 +144,8 @@ public class Solution{
         return slow;
     }
 }
+
+
+/*
+The main idea is the same with problem Linked List Cycle II,https://leetcode.com/problems/linked-list-cycle-ii/. Use two pointers the fast and the slow. The fast one goes forward two steps each time, while the slow one goes only step each time. They must meet the same item when slow==fast. In fact, they meet in a circle, the duplicate number must be the entry point of the circle when visiting the array from nums[0]. Next we just need to find the entry point. We use a point(we can use the fast one before) to visit form begining with one step each time, do the same job to slow. When fast==slow, they meet at the entry point of the circle. The easy understood code is as follows.
+*/
