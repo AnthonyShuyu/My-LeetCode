d59b349ba1f67500fe295ff6d32052c3a0e7f623
Yu Shu <ys2530@nyu.edu>
2016-07-01 01:15:47 -0400

    [136. Single Number][Runtime Error]committed by Anthony

---
+++ 136-Single-Number/solution.java
@@ -0,0 +1,28 @@
+/**
+ * 136. Single Number
+ * 
+ */ 
+
+
+
+// s1: Arrays.sort()
+public class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        Arrays.sort(nums);
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        }
+        if (nums[nums.length - 1] != nums[nums.length - 2]) {
+            return nums[nums.length - 1];
+        }
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
