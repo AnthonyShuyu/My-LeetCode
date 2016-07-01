640a525a0527b3abc9928b207efe920bbe445371
Yu Shu <ys2530@nyu.edu>
2016-07-01 16:09:59 -0400

    [137. Single Number II][Accepted]committed by Anthony

--- 137-Single-Number-II/solution.java
+++ 137-Single-Number-II/solution.java
@@ -34,9 +34,10 @@ public class Solution {
 }
 */
 
-// s3: Arrays.sort()
+// s2: Arrays.sort()
 // O(nlogn), O(1)
 
+/*
 public class Solution{
     public int singleNumber(int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -58,5 +59,29 @@ public class Solution{
             }
         }
         return -1;
+    }
+}
+*/
+
+
+// s3: tricky, bitwise operation ?
+// O(n), O(1)
+
+public class Solution{
+    public int singleNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int result = 0;
+        int[] bits = new int[32];
+        
+        for (int i = 0; i < 32; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                bits[i] = bits[i] + ((nums[j] >> i) & 1);
+                bits[i] = bits[i] % 3;                
+            }
+            result = result | bits[i] << i;
+        }
+        return result;
     }
 }
