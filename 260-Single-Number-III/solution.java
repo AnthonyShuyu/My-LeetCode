14eebf8dd94f2be2013e8a5e50d4c363b651d49d
Yu Shu <ys2530@nyu.edu>
2016-07-01 16:29:49 -0400

    [260. Single Number III][Accepted]committed by Anthony

--- 260-Single-Number-III/solution.java
+++ 260-Single-Number-III/solution.java
@@ -9,12 +9,12 @@
 // s1: brute force
 // O(n^2), O(1)
 
-
+/*
 public class Solution {
     public int[] singleNumber(int[] nums) {
         int[] result = new int[2];
         if (nums == null || nums.length == 0) {
-            return result;
+            return null;
         }
         int index = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -35,8 +35,44 @@ public class Solution {
         }
         return result;
     }
-    
-    
-    
-    
+}
+
+*/
+
+
+// s2: Arrays.sort()
+// O(nlogn), O(1)
+
+public class Solution{
+    public int[] singleNumber(int[] nums) {
+        int[] result = new int[2];
+        if (nums == null || nums.length <= 1) {
+            return null;
+        }
+        int index = 0;
+        Arrays.sort(nums);
+        if (nums.length == 2) {
+            if (nums[0] != nums[1]) {
+                return nums;
+            } else {
+                return null;
+            }
+        }
+        if (nums[0] != nums[1]) {
+            result[index] = nums[0];
+            index++;
+        }
+        if (nums[nums.length - 1] != nums[nums.length - 2]) {
+            result[index] = nums[nums.length - 1];
+            index++;
+        }
+        
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
+                result[index] = nums[i];
+                index++;
+            }
+        }
+        return result;
+    }
 }
