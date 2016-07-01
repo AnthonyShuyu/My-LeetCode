c9159ffc4bb6119d1a5526445f41a7247d99d829
Yu Shu <ys2530@nyu.edu>
2016-07-01 00:56:52 -0400

    [41. First Missing Positive][Accepted]committed by Anthony

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -73,9 +73,9 @@ public class Solution{
                 
                 int temp = nums[nums[i] - 1];
                 
-                // if (temp == nums[i]) {
-                //     break;
-                // }
+                if (temp == nums[i]) {
+                    break;
+                }
                 
                 nums[nums[i] - 1] = nums[i];
                 nums[i] = temp;
