b2318a361d96b0e47468e77ce73919b6183b0a30
Yu Shu <ys2530@nyu.edu>
2016-07-02 00:18:18 -0400

    [152. Maximum Product Subarray][Accepted]committed by Anthony

--- 152-Maximum-Product-Subarray/solution.java
+++ 152-Maximum-Product-Subarray/solution.java
@@ -1,14 +1,53 @@
 /**
  * 
  * 152. Maximum Product Subarray
+ * 1 solution
  * 
  */
 
+public class Solution{
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[] max = new int[n];
+        int[] min = new int[n];
+        
+        max[0] = min[0] = nums[0];
+        int result = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            max[i] = min[i] = nums[i];
+            if (nums[i] > 0) {
+                max[i] = Math.max(max[i], max[i - 1] * nums[i]);
+                min[i] = Math.min(min[i], min[i - 1] * nums[i]);
+            } else if (nums[i] < 0) {
+                max[i] = Math.max(max[i], min[i - 1] * nums[i]);
+                min[i] = Math.min(min[i], max[i - 1] * nums[i]);
+            }
+            result = Math.max(result, max[i]);
+        }
+        return result;
+    }
+}
 
 
-// s1: tricky, two variables, one to remember the best answer
+
+
+
+
+
+
+
+
+
+
+// s2: tricky, two variables, one to remember the best answer
 // O(n), O(1)
+// failed
 
+/*
 public class Solution{
     public int maxProduct(int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -27,10 +66,11 @@ public class Solution{
         return max;
     }
 }
+*/
 
 
 
-// s2: Greedy
+// s3: Greedy
 // O(n), O(1)
 // failed, because negative times negative will get a positive
 
