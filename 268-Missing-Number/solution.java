aee5daa4ca26e0ad68752dccbee78b790c0006e8
Yu Shu <ys2530@nyu.edu>
2016-07-01 21:03:17 -0400

    [268. Missing Number][Accepted]committed by Anthony

---
+++ 268-Missing-Number/solution.java
@@ -0,0 +1,27 @@
+/**
+ * 268. Missing Number
+ * 
+ * 
+ */ 
+
+
+
+
+public class Solution {
+    public int missingNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        Arrays.sort(nums);
+        if (nums[0] != 0) {
+            return 0;
+        }
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] + 1 != nums[i + 1]) {
+                return nums[i] + 1;
+            }
+        }
+        return nums.length;
+    }
+}
