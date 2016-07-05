78cde0381cb8d2463220c8bf1eba4c779d5bc8a2
Yu Shu <ys2530@nyu.edu>
2016-07-05 14:54:12 -0400

    [15. 3Sum][Time Limit Exceeded]committed by Anthony

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -11,13 +11,10 @@
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        
         if (nums == null || nums.length == 0) {
             return result;
         }
-        
         Arrays.sort(nums);
-        
         for (int i = 0; i < nums.length - 2; i++) {
             if (i != 0 && nums[i] == nums[i - 1]) {
                 continue;
