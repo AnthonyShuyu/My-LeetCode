793edb42d5a37cacc606094b711920f3663ceced
Yu Shu <ys2530@nyu.edu>
2016-07-04 22:24:21 -0400

    [15. 3Sum][Compile Error]committed by Anthony

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -1,6 +1,6 @@
 /**
  * 15. 3Sum
- * 
+ * 2 solutions
  * 
  */
 
@@ -36,6 +36,44 @@ public class Solution {
                 } else {
                     hashMap.put(nums[j], j);
                 }
+            }
+        }
+        return result;
+    }
+}
+
+
+
+
+// s2: two pointers, convert to the problem of 2 sum
+// O(n^2), O(n^2)
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        Arrays.sort(nums);
+         
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i != 0 && nums[i] != nums[i - 1]) {
+                continue;
+            }
+            int target = 0 - nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    if (!result.contains(list)) {
+                        result.add(list);
+                    }
+                }
+                
             }
         }
         return result;
