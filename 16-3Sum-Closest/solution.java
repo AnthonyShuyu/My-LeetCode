305ebf8e10f0761adb1e514131c7dc0474fcada5
Yu Shu <ys2530@nyu.edu>
2016-07-05 01:16:07 -0400

    [16. 3Sum Closest][Time Limit Exceeded]committed by Anthony

--- 16-3Sum-Closest/solution.java
+++ 16-3Sum-Closest/solution.java
@@ -14,6 +14,7 @@ public class Solution {
             return -1;
         }
         int min = Integer.MAX_VALUE;
+        int result = 0;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
