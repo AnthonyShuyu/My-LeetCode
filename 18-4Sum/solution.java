78fb36fca4d4bf71d6a083b7b956f019af047af7
Yu Shu <ys2530@nyu.edu>
2016-07-05 15:45:37 -0400

    [18. 4Sum][Accepted]committed by Anthony

--- 18-4Sum/solution.java
+++ 18-4Sum/solution.java
@@ -1,13 +1,14 @@
 /**
- * 
  * 18. 4Sum
  * 
  */
 
 
 // s1: brute force
-// 
+// O(n^4), O(n)
+// time limit exceeded
 
+/*
 public class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
@@ -38,6 +39,53 @@ public class Solution {
             }
         }
         return result;
-        
+    }
+}
+*/
+
+
+
+// s2:
+// 
+
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length < 4) {
+            return result;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                // if (j != 0 && nums[j] == nums[j - 1]) {
+                //     continue;
+                // }
+               int start = j + 1;
+               int end = nums.length - 1;
+               while (start < end) {
+                   int sum = nums[i] + nums[j] + nums[start] + nums[end];
+                   if (sum == target) {
+                       List<Integer> list = new ArrayList<Integer>();
+                       list.add(nums[i]);
+                       list.add(nums[j]);
+                       list.add(nums[start]);
+                       list.add(nums[end]);
+                       if (!result.contains(list)) {
+                           result.add(list);
+                       }
+                       start++;
+                       end--;
+                   } else if (sum < target) {
+                       start++;
+                   } else {
+                       end--;
+                   }
+               }
+            }
+        }
+        return result;
     }
 }
