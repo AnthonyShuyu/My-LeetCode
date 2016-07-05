24b53a36f99318464d28f71fc5f9275e66424569
Yu Shu <ys2530@nyu.edu>
2016-07-05 14:55:36 -0400

    [15. 3Sum][Accepted]committed by Anthony

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -15,7 +15,7 @@ public class Solution {
             return result;
         }
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (i != 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
