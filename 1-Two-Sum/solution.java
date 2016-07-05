0832359e0576e1cf7955a3c7289e07510cb4d957
Yu Shu <ys2530@nyu.edu>
2016-07-04 20:13:25 -0400

    [1. Two Sum][Compile Error]committed by Anthony

--- 1-Two-Sum/solution.java
+++ 1-Two-Sum/solution.java
@@ -8,6 +8,7 @@
 // s1: brute force
 // O(n^2), O(1)
 
+/*
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
@@ -26,17 +27,10 @@ public class Solution {
         return result;
     }
 }
+*/
 
 
-
-
-
-
-
-
-
-
-// s1: HashMap
+// s2: HashMap
 // O(n), O(n)
 
 /*
@@ -62,8 +56,9 @@ public class Solution {
 */
 
 
-// s2: two pointers
+// s3: two pointers
 // O(nlogn), O(1)
+// failed, can't use sort here, cause the question asked for the indices, not the numbers
 
 /*
 public class Solution {
@@ -72,13 +67,18 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             return result;
         }
+        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            hashMap.put(nums[i], i);
+        }
+        
         Arrays.sort(nums);
         int left = 0;
         int right = nums.length - 1;
         while (left < right) {
             if (nums[left] + nums[right] == target) {
-                result[0] = left;
-                result[1] = right;
+                result[0] = hashMap.get(nums[left]);
+                result[1] = hashMap.get(nums[right]);
                 return result;
             } else if (nums[left] + nums[right] < target) {
                 left++;
@@ -90,4 +90,3 @@ public class Solution {
     }
 }
 */
-
