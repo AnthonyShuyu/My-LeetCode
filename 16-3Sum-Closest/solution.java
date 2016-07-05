48918e01e5bf837d6e9ab1f51ab454de27053baf
Yu Shu <ys2530@nyu.edu>
2016-07-05 01:15:25 -0400

    [16. 3Sum Closest][Compile Error]committed by Anthony

---
+++ 16-3Sum-Closest/solution.java
@@ -0,0 +1,30 @@
+/**
+ * 16. 3Sum Closest
+ * 
+ * 
+ */
+
+
+// s1: brute force
+// O(n^3), 
+
+public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    // min = Math.min(min, Math.abs(nums[i] + nums[j] + nums[k] - target));  
+                    if (Math.min(nums[i] + nums[j] + nums[k] - target) < min) {
+                        result = nums[i] + nums[j] + nums[k];
+                        min = Math.min(nums[i] + nums[j] + nums[k] - target);
+                    }
+                }
+            }
+        }
+        return result; 
+    }
+}
