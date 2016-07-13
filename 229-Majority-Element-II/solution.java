09ea9d1e83a84b6983b256b664f3d2c7734ca2e5
Yu Shu <ys2530@nyu.edu>
2016-07-13 15:27:00 -0400

    [229. Majority Element II][Compile Error]committed by Anthony

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -43,6 +43,7 @@ public class Solution {
 // s2: HashMap
 // O(n), O(n)
 
+/*
 public class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> result = new ArrayList<Integer>();
@@ -64,5 +65,57 @@ public class Solution {
             }
         }
         return result;        
+    }
+}
+*/
+
+// *s3: one for loop & use count and two candidates
+// O(n), O(1)
+// tricky
+
+public class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        int count1 = 0;
+        int count2 = 0;
+        int candidate1 = nums[i];
+        int candidate2 = nums[i];
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (count1 == 0) {
+                candidate1 = nums[i];
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = nums[i];
+                count2 = 1;
+            } else if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        count1 = 0;
+        count2 = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == candidate1) {
+                count1++;
+            } else if (nums[i] == candidate2) {
+                count2++;
+            }
+        }
+        if (count1 > nums.length / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > nums.length / 3) {
+            result.add(candidate2);
+        }
+        return result;
     }
 }
