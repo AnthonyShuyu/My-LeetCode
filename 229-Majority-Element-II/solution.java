e62eb0751e161e4168a03f53f7807233c1346c51
Yu Shu <ys2530@nyu.edu>
2016-07-13 15:13:48 -0400

    [229. Majority Element II][Accepted]committed by Anthony

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -7,7 +7,9 @@
 
 // s1: brute force
 // O(n^2), O(1)
+// time limit exceeded
 
+/*
 public class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> result = new ArrayList<Integer>();
@@ -33,5 +35,34 @@ public class Solution {
             }
         }
         return result;
+    }
+}
+*/
+
+
+// s2: HashMap
+// O(n), O(n)
+
+public class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(nums[i])) {
+                hashMap.put(nums[i], hashMap.get(nums[i]) + 1);
+            } else {
+                hashMap.put(nums[i], 1);
+            }
+            
+            if (hashMap.get(nums[i]) > nums.length / 3) {
+                if (!result.contains(nums[i])) {
+                    result.add(nums[i]);
+                }
+            }
+        }
+        return result;        
     }
 }
