6e23d6d04ae251869363b5d794b2abf86ad1d164
Yu Shu <ys2530@nyu.edu>
2016-07-13 13:13:59 -0400

    [76. Minimum Window Substring][Compile Error]committed by Anthony

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -45,10 +45,12 @@ public class Solution {
 */
 
 
-// s2: 
-// 
+// s2: brute force, use a little tricky
+// O(n^3), O(n^2)
+// time limit exceeded
 
 
+/*
 public class Solution {
     public String minWindow(String s, String t) {
         if (s == null || t == null || s.length() < t.length()) {
@@ -88,3 +90,58 @@ public class Solution {
         return true;
     }
 }
+
+*/
+
+// s3: 
+
+
+
+
+public class Solution {
+    //方法一:
+    int initTargetHash(int []targethash, String Target) {
+        int targetnum =0 ;
+        for (char ch : Target.toCharArray()) {
+            targetnum++;
+            targethash[ch]++;
+        }
+        return targetnum;
+    }
+    boolean valid(int []sourcehash, int []targethash) {
+        
+        for(int i = 0; i < 256; i++) {
+            if(targethash[i] > sourcehash[i])    
+                return false;
+        }
+        return true;
+    }
+    public String minWindow(String Source, String Target) {
+        // queueing the position that matches the char in T
+        int ans = Integer.MAX_VALUE;
+        String minStr = "";
+        
+        int[] sourcehash = new int[256];
+        int[] targethash = new int[256];
+        
+        initTargetHash(targethash, Target);
+        int j = 0, i = 0;
+        for(i = 0; i < Source.length(); i++) {
+            
+            while( !valid(sourcehash, targethash) && j < Source.length()  ) {
+                sourcehash[Source.charAt(j)]++;
+                if(j < Source.length() )
+                    j++;
+                else 
+                    break;
+            }
+            if(valid(sourcehash, targethash) ){
+                if(ans > j - i ) {
+                    ans = Math.min(ans, j - i );
+                    minStr = Source.substring(i, j );
+                }
+            }
+            sourcehash[Source.charAt(i)]--;
+        }
+        return minStr;
+    }
