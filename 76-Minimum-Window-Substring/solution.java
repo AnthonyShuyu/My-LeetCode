17c38bbbb93a678ec0268b49194c8137c805ee20
Yu Shu <ys2530@nyu.edu>
2016-07-13 12:51:18 -0400

    [76. Minimum Window Substring][Time Limit Exceeded]committed by Anthony

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -12,11 +12,11 @@ public class Solution {
         if (s == null || t == null) {
             return "";
         }    
-        if (s.size() < t.size()) {
+        if (s.length() < t.length()) {
             return "";
         }
-        for (int i = t.size(); i <= s.size(); i++) {
-            for (int j = 0; j <= s.size() - i ; j++) {
+        for (int i = t.length(); i <= s.length(); i++) {
+            for (int j = 0; j <= s.length() - i ; j++) {
                 if (checkContains(s.substring(j, j + i), t)) {
                     return s.substring(j, j + i);
                 }
@@ -27,10 +27,10 @@ public class Solution {
     
     public boolean checkContains(String s1, String s2) {
         int[] arr = new int[58]; // 'z' - 'A' + 1
-        for (int i = 0; i < s1.size(); i++) {
+        for (int i = 0; i < s1.length(); i++) {
             arr[(int)s1.charAt(i) - 65] += 1;
         }
-        for (int i = 0; i < s2.size(); i++) {
+        for (int i = 0; i < s2.length(); i++) {
             arr[(int)s2.charAt(i) - 65] -= 1;
             if (arr[(int)s2.charAt(i) - 65] < 0) {
                 return false;
