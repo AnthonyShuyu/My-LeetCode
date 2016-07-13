fde3efe8436083ad3cffaf9ca5bdfce1d3797e61
Yu Shu <ys2530@nyu.edu>
2016-07-13 15:32:06 -0400

    [229. Majority Element II][Accepted]committed by Anthony

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -81,20 +81,20 @@ public class Solution {
         }
         int count1 = 0;
         int count2 = 0;
-        int candidate1 = nums[i];
-        int candidate2 = nums[i];
+        int candidate1 = nums[0];
+        int candidate2 = nums[0];
         
         for (int i = 0; i < nums.length; i++) {
-            if (count1 == 0) {
+            if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            } else if (count1 == 0) {
                 candidate1 = nums[i];
                 count1 = 1;
             } else if (count2 == 0) {
                 candidate2 = nums[i];
                 count2 = 1;
-            } else if (candidate1 == nums[i]) {
-                count1++;
-            } else if (candidate2 == nums[i]) {
-                count2++;
             } else {
                 count1--;
                 count2--;
