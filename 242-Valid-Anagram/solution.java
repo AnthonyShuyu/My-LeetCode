143f5f795f6e5205b4d7f7f37c208bed5eb8416f
Yu Shu <ys2530@nyu.edu>
2016-07-14 13:51:54 -0400

    [242. Valid Anagram][Accepted]committed by Anthony

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -57,16 +57,57 @@ public class Solution {
 */
 
 
-// s3: HashMap, use 2 HashMaps, and loop traverse them, to see if they are alike
+// s3: HashMap, use one HashMap
 // O(n), O(n)
-// easy to think, hard to implement
+
 
 public class Solution {
     public boolean isAnagram(String s, String t) {
         if (s == null || t == null) {
             return false;
+        }
+        Map<Character, Integer> hashMap = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            if (hashMap.containsKey(s.charAt(i))) {
+                hashMap.put(s.charAt(i), hashMap.get(s.charAt(i)) + 1);
+            } else {
+                hashMap.put(s.charAt(i), 1);
+            }
+        }
+        
+        for (int i = 0; i < t.length(); i++) {
+            if (!hashMap.containsKey(t.charAt(i))) {
+                return false;
+            } else {
+                hashMap.put(t.charAt(i), hashMap.get(t.charAt(i)) - 1);
+                if (hashMap.get(t.charAt(i)) < 0) {
+                    return false;
+                }
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : hashMap.entrySet()) {
+            if (entry.getValue() != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
+// s3: HashMap, use 2 HashMaps, and loop traverse them, to see if they are alike ?
+// O(n), O(n)
+// easy to think, hard to implement, Wrong answer?
+
+/*
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null) {
+            return false;
         }    
-        Map<Character, Long> hashMap1 = new HashMap<Character, Long>();
+        Map<Character, Integer> hashMap1 = new HashMap<Character, Integer>();
         for (int i = 0; i < s.length(); i++) {
             if (hashMap1.containsKey(s.charAt(i))) {
                 hashMap1.put(s.charAt(i), hashMap1.get(s.charAt(i)) + 1);
@@ -75,7 +116,7 @@ public class Solution {
             }
         }
         
-        Map<Character, Long> hashMap2 = new HashMap<Character, Long>();
+        Map<Character, Integer> hashMap2 = new HashMap<Character, Integer>();
         for (int i = 0; i < t.length(); i++) {
             if (hashMap2.containsKey(t.charAt(i))) {
                 hashMap2.put(t.charAt(i), hashMap2.get(t.charAt(i)) + 1);
@@ -89,12 +130,12 @@ public class Solution {
             return false;
         }
         
-        for (Map.Entry<Character, Long> entry: hashMap1.entrySet()) {
-            char c = entry.getKey();
-            if (hashMap2.get(c) != entry.getValue()) {
+        for (Map.Entry<Character, Integer> entry: hashMap1.entrySet()) {
+            if (hashMap2.get(entry.getKey()) != entry.getValue()) {
                 return false;
             }  
         }
         return true;
     }
 }
+ */
