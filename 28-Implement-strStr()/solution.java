fac4ebfbf745e44bb6102f7253ab62a850e5c8d7
Yu Shu <ys2530@nyu.edu>
2016-07-14 00:23:06 -0400

    [28. Implement strStr()][Runtime Error]committed by Anthony

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -31,10 +31,8 @@ public class Solution {
         if (haystack == null || needle == null || haystack.length() < needle.length()) {
             return -1;
         }
-        if (needle.length() == 0) {
-            return -1;
-        }
-        for (int i = 0; i < haystack.length(); i++) {
+
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
             if (haystack.charAt(i) == needle.charAt(0)) {
                 int index = 1;
                 while (index < needle.length()) {
