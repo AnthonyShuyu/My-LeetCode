1ceacd574a2237ef0ba150e7fbdab35c9a7aa3d5
Yu Shu <ys2530@nyu.edu>
2016-07-14 13:16:00 -0400

    [242. Valid Anagram][Compile Error]committed by Anthony

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -9,6 +9,7 @@
 // s1: Arrays.sort()
 // O(nlogn), O(n)
 
+/*
 public class Solution {
     public boolean isAnagram(String s, String t) {
         if (s == null || t == null) {
@@ -21,3 +22,29 @@ public class Solution {
         return String.valueOf(sArray).equals(String.valueOf(tArray));
     }
 }
+*/
+
+
+// s2: count the characters
+// O(n), O(n)
+// tricky
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null) {
+            return false;
+        }
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[(int)(s.charAt(i) - 'a')] += 1;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            arr[(int)(t.charAt(i) - 'a')] -= 1;
+            if (arr[(int)(t.charAt(i) - a)] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
