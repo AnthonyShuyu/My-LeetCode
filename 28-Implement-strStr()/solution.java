7b2e1a07741adda4874a2610bc450cfb8a746ac9
Yu Shu <ys2530@nyu.edu>
2016-07-14 00:25:16 -0400

    [28. Implement strStr()][Accepted]committed by Anthony

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -28,25 +28,38 @@ public class Solution {
 
 public class Solution {
     public int strStr(String haystack, String needle) {
-        if (haystack == null || needle == null || haystack.length() < needle.length()) {
+        if (haystack == null || needle == null) {
             return -1;
         }
 
+
+        // for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+        //     if (haystack.charAt(i) == needle.charAt(0)) {
+        //         int index = 1;
+        //         while (index < needle.length()) {
+        //             if (haystack.charAt(i + index) == needle.charAt(index)) {
+        //                 index++;
+        //                 if (index == needle.length()) {
+        //                     return i;
+        //                 }
+        //             } else {
+        //                 break;
+        //             }
+        //         }
+        //     } else {
+        //         continue;
+        //     }
+        // }
+        
         for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
-            if (haystack.charAt(i) == needle.charAt(0)) {
-                int index = 1;
-                while (index < needle.length()) {
-                    if (haystack.charAt(i + index) == needle.charAt(index)) {
-                        index++;
-                        if (index == needle.length()) {
-                            return i;
-                        }
-                    } else {
-                        break;
-                    }
+            int j = 0;
+            for (j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
                 }
-            } else {
-                continue;
+            }
+            if (j == needle.length()) {
+                return i;
             }
         }
         return -1;
