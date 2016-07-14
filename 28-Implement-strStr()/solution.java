43744cc1196d0d05131eb6a60f35ea620103d33f
Yu Shu <ys2530@nyu.edu>
2016-07-14 00:19:43 -0400

    [28. Implement strStr()][Compile Error]committed by Anthony

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -20,3 +20,32 @@ public class Solution {
         return -1;
     }
 }
+
+// s2: String.charAt()
+// O(n), O(1)
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null || haystack.length() < needle.length()) {
+            return -1;
+        }
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                int index = 1;
+                while (index < needle.length()) {
+                    if (haystack.charAt(i + index) == needle.charAt(index)) {
+                        index++;
+                        if (index == needle.length()) {
+                            return i;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                continue;
+            }
+        }
+        return -1;
+    }
+}
