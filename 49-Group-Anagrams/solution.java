2169bad7d1efa30bf795602e98c835a4fe07f2f2
Yu Shu <ys2530@nyu.edu>
2016-07-14 14:07:53 -0400

    [49. Group Anagrams][Time Limit Exceeded]committed by Anthony

---
+++ 49-Group-Anagrams/solution.java
@@ -0,0 +1,53 @@
+/**
+ * 49. Group Anagrams
+ * 
+ * 
+ */
+
+
+// s1: brute force
+//
+
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        if (strs == null || strs.length == 0) {
+            return result;
+        }
+        Set<String> hashSet = new HashSet<String>();
+        for (int i = 0; i < strs.length; i++) {
+            if (!hashSet.contains(strs[i])) {
+                List<String> stringList = new ArrayList<String>();
+                String s = strs[i];
+                stringList.add(s);
+                for (int j = i + 1; j < strs.length; j++) {
+                    if (isAnagram(s, strs[j])) {
+                        stringList.add(strs[j]);
+                        hashSet.add(strs[j]);
+                    }
+                }
+                result.add(stringList);
+            }
+        }
+        return result;
+    }
+    
+    public boolean isAnagram(String s, String t) {
+        char[] arr = new char[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[(int)(s.charAt(i) - 'a')] += 1;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            arr[(int)(t.charAt(i) - 'a')] -= 1;
+            if (arr[(int)(t.charAt(i) - 'a')] < 0) {
+                return false;
+            } 
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
