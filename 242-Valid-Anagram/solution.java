2489e45d5addad39b6fe32f932ef38b7746c8e81
Yu Shu <ys2530@nyu.edu>
2016-07-14 13:33:17 -0400

    [242. Valid Anagram][Compile Error]committed by Anthony

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -29,6 +29,7 @@ public class Solution {
 // O(n), O(n)
 // tricky
 
+/*
 public class Solution {
     public boolean isAnagram(String s, String t) {
         if (s == null || t == null) {
@@ -53,4 +54,47 @@ public class Solution {
         return true;
     }
 }
+*/
 
+
+// s3: HashMap, use 2 HashMaps, and loop traverse them, to see if they are alike
+// O(n), O(n)
+// easy to think, hard to implement
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null) {
+            return false;
+        }    
+        Map<Character, Integer> hashMap1 = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            if (hashMap1.containsKey(s.charAt(i)) {
+                hashMap1.put(s.charAt(i), hashMap1.get(s.charAt(i)) + 1);
+            } else {
+                hashMap1.put(s.charAt(i), 1);
+            }
+        }
+        
+        Map<Character, Integer> hashMap2 = new HashMap<Character, Integer>();
+        for (int i = 0; i < t.length(); i++) {
+            if (hashMap2.containsKey(t.charAt(i)) {
+                hashMap2.put(t.charAt(i), hashMap2.get(t.charAt(i)) + 1);
+            } else {
+                hashMap2.put(t.charAt(i), 1);
+            }
+        }
+        
+        // loop traverse the two HashMaps to see if they are the same
+        if (hashMap1.size() != hashMap2.size()) {
+            return false;
+        }
+        
+        for (Map.Entry<Character, Integer> entry: hashMap1.entrySet()) {
+            Char c = entry.getKey();
+            if (hashMap2.get(entry.getKey()) != entry.getValue()) {
+                return false;
+            }  
+        }
+        return true;
+    }
+}
