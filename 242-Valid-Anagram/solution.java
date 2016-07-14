fd92dda00856993b55d1182f21df1fb533780c5c
Yu Shu <ys2530@nyu.edu>
2016-07-14 13:53:01 -0400

    [242. Valid Anagram][Runtime Error]committed by Anthony

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -85,8 +85,13 @@ public class Solution {
                 }
             }
         }
-        for (Map.Entry<Character, Integer> entry : hashMap.entrySet()) {
-            if (entry.getValue() != 0) {
+        // for (Map.Entry<Character, Integer> entry : hashMap.entrySet()) {
+        //     if (entry.getValue() != 0) {
+        //         return false;
+        //     }
+        // }
+        for (int i = 0; i < hashMap.size(); i++) {
+            if (hashMap.get(i) != 0) {
                 return false;
             }
         }
