2e02958e265e5c291baf8fac4014a6e1822d373b
Yu Shu <ys2530@nyu.edu>
2016-07-15 00:18:45 -0400

    [165. Compare Version Numbers][Compile Error]committed by Anthony

--- 165-Compare-Version-Numbers/solution.java
+++ 165-Compare-Version-Numbers/solution.java
@@ -4,21 +4,62 @@
  * 
  */
 
+// s1: string.split() method
+// O(n), O(n)
+// tricky
 
+public class Solution {
+    public int compareVersion(String version1, String version2) {
+        if (version1 == null || version2 == null || version1.length() == 0 || version2.length() == 0) {
+            return 0;
+        }
+        String[] strings1 = version1.split("\\.");
+        String[] strings2 = version2.split("\\.");
+        int max = Math.max(strings1.length, Strings2.length);
+        for (int i = 0; i < max; i++) {
+            int num1, num2;
+            if (i < strings1.length) {
+                num1 = Integer.valueOf(strings1[i]);
+            } else {
+                nums1 = 0;
+            }
+            
+            if (i < strings2.length) {
+                num2 = Integer.valueOf(strings2[i]);
+            } else {
+                nums2 = 0;
+            }
+            if (nums1 > nums2) {
+                return 1;
+            } else if (nums1 < nums2) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+}
+
+
+
+
+
+
+
+
+/*
 public class Solution {
     public int compareVersion(String version1, String version2) {
         if (version1 == null || version1.length() == 0 || version2 == null|| version2.length() == 0) {
             return 0;
         }
-        String[] string1 = version1.split(".");
-        String[] string2 = version2.split(".");
         
+        String[] string1 = version1.split("\\.");
+        String[] string2 = version2.split("\\.");
         int n = string1.length < string2.length ? string1.length : string2.length;
-        
         for (int i = 0;i < n; i++) {
             if (Integer.valueOf(string1[i]) < Integer.valueOf(string2[i])) {
                 return -1;                
-            } else if (Integer.valueOf(string[i]) > Integer.valueOf(string2[i])) {
+            } else if (Integer.valueOf(string1[i]) > Integer.valueOf(string2[i])) {
                 return 1;
             }
         }
@@ -30,5 +71,36 @@ public class Solution {
             return 0;
         }
         
+        // int index = 0;
+        // int index2 = 0;
+        // int flag = 0;
+        // while (index < version1.length() && index < version2.length()) {
+        //     if (version1.charAt(index2) < version2.charAt(index2)) {
+        //       flag = -1; 
+        //     } else if (version1.charAt(index2) > version2.charAt(index2)) {
+        //         flag = 1;
+        //     }
+        //     if (version1.charAt(index) == '.' && version2.charAt(index) != '.') {
+        //         return 1;
+        //     } else if (version1.charAt(index) != '.' && version2.charAt(index) == '.') {
+        //         return -1;
+        //     } else if (version1.charAt(index) == '.' && version2.charAt(index) == '.'){
+        //         if (flag != 0) {
+        //             return flag;
+        //         } else {
+        //             index2 = index + 1;
+        //         }
+        //     }
+        //     index++;
+        // }
+        
+        // if (version1.length() > version2.length()) {
+        //     return 1;
+        // } else if (version1.length() < version2.length()){
+        //     return -1;
+        // } else {
+        //     return flag;
+        // }
     }
 }
+*/
