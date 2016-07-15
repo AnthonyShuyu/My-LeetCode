97635ba01bef6bf99e1ea32a8edc97e7bcee64e4
Yu Shu <ys2530@nyu.edu>
2016-07-15 14:29:19 -0400

    [12. Integer to Roman][Accepted]committed by Anthony

--- 12-Integer-to-Roman/solution.java
+++ 12-Integer-to-Roman/solution.java
@@ -7,9 +7,6 @@
 // Ask question: what is roman numeral?
 // any of the letters representing numbers in the Roman numerical system: I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1,000. In this system, a letter placed after another of greater value adds (thus XVI or xvi is 16), whereas a letter placed before another of greater value subtracts (thus XC or xc is 90).
 
-
-
-
 // s1: two arrays
 // O(n), O(n)
 
@@ -41,7 +38,7 @@ public class Solution {
             for (int i = 0; i < times; i++) {
                 sb.append(hashMap.get(nums[index]));
             }
-            num = times * nums[index];
+            num -= times * nums[index];
             index++;
         }
         return sb.toString();
