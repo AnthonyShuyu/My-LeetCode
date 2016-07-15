0c8c1f54902bccb2804e01dbb8b42d06d36d8e98
Yu Shu <ys2530@nyu.edu>
2016-07-15 16:31:53 -0400

    [283. Move Zeroes][Compile Error]committed by Anthony

--- 283-Move-Zeroes/solution.java
+++ 283-Move-Zeroes/solution.java
@@ -4,8 +4,9 @@
  * 
  */
 
-// s1: one traverse, when meet not zero, move it to the beginning
+// *s1: one traverse, when meet not zero, move it to the beginning
 // O(n), O(1)
+// tricky
 
 public class Solution {
     public void moveZeroes(int[] nums) {
@@ -21,6 +22,33 @@ public class Solution {
         }
         for (int i = index; i < nums.length; i++) {
             nums[i] = 0;
+        }
+    }
+}
+
+
+// s2: brute force, swap elements
+// O(n^2), O(1)
+
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                continue;
+            } else {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] == 0) {
+                        continue;
+                    } else {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
         }
     }
 }
