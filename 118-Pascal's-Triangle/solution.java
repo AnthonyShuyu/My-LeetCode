436b3692c8b0418e1a765553411861aad6efc4f9
Yu Shu <ys2530@nyu.edu>
2016-07-16 16:16:12 -0400

    [118. Pascal's Triangle][Accepted]committed by Anthony

---
+++ 118-Pascal's-Triangle/solution.java
@@ -0,0 +1,35 @@
+/**
+ * 118. Pascal's Triangle
+ * 
+ * 
+ */
+
+
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (numRows <= 0) {
+            return result;
+        }
+        // initaite
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> list = new ArrayList<Integer>();
+            for (int j = 0; j < i + 1; j++) {
+                list.add(1);                
+            }
+            result.add(list);
+        }
+        
+        // set the middle nums
+        for (int i = 2; i < numRows; i++) {
+            List<Integer> last_list = result.get(i - 1);
+            List<Integer> current_list = result.get(i);
+            for (int j = 1; j < i; j++) {
+                current_list.set(j, last_list.get(j - 1) + last_list.get(j));
+            }
+        }
+        
+        return result;
+        
+    }
+}
