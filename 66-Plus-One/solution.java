a7f3c5073a854bff023b22e32ea35f437a79cbfa
Yu Shu <ys2530@nyu.edu>
2016-07-16 22:50:49 -0400

    [66. Plus One][Compile Error]committed by Anthony

--- 66-Plus-One/solution.java
+++ 66-Plus-One/solution.java
@@ -3,7 +3,6 @@
  * 
  */
 
-
 // s1: brute force
 // O(n), O(n)
 
@@ -38,5 +37,39 @@ public class Solution {
             result[index] = digits[index] + 1;
         }
         return result;
+    }
+}
+
+
+
+
+// *s2: use carry
+// O(1), O(1)   
+    // The complexity is O(1)
+    // f(n) = 9/10 * 1 + 1/10 * O(n-1)
+    //  ==>  O(n) =  10 / 9 = 1.1111 = O(1)
+// tricky
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        if (digits == null || digits.length == 0) {
+            return null;
+        }
+        int carry = 1;
+        for (int i = digits.length - 1; i >= 0 && carry == 1; i--) {
+            int sum = digits[i] + 1;
+            digits[i] = sum % 10;
+            carry = sum / 10;
+        }
+        
+        if (carry == 0) {
+            return digits;
+        } else {
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            return result;
+        }
+                
+        
     }
 }
