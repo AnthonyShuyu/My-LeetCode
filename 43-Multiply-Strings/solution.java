8c3acb960e983ec377d091fd90dcc57f526c0505
Yu Shu <ys2530@nyu.edu>
2016-07-17 18:30:44 -0400

    [43. Multiply Strings][Accepted]committed by Anthony

--- 43-Multiply-Strings/solution.java
+++ 43-Multiply-Strings/solution.java
@@ -8,14 +8,53 @@
 
 
 
+// public class Solution {
+//     public String multiply(String num1, String num2) {
+//         if (num1 == null || num1.length() == 0 || num2 == null || num2.length() == 0) {
+//             return null;
+//         }
+//         long long_num1 = Long.parseLong(num1);
+//         long long_num2 = Long.parseLong(num2);
+//         long result = long_num1 * long_num2;
+//         return String.valueOf(result);
+//     }
+// }
+
+
 public class Solution {
     public String multiply(String num1, String num2) {
-        if (num1 == null || num1.length() == 0 || num2 == null || num2.length() == 0) {
+        if (num1 == null || num2 == null) {
             return null;
         }
-        long long_num1 = Long.parseLong(num1);
-        long long_num2 = Long.parseLong(num2);
-        long result = long_num1 * long_num2;
-        return String.valueOf(result);
+        
+        int len1 = num1.length(), len2 = num2.length();
+        int len3 = len1 + len2;
+        int i, j, product, carry;
+
+        int[] num3 = new int[len3];
+        for (i = len1 - 1; i >= 0; i--) {
+            carry = 0;
+            for (j = len2 - 1; j >= 0; j--) {
+                product = carry + num3[i + j + 1] +
+                    Character.getNumericValue(num1.charAt(i)) *
+                    Character.getNumericValue(num2.charAt(j));
+                num3[i + j + 1] = product % 10;
+                carry = product / 10;
+            }
+            num3[i + j + 1] = carry;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        i = 0;
+
+        while (i < len3 - 1 && num3[i] == 0) {
+            i++;
+        }
+
+        while (i < len3) {
+            sb.append(num3[i++]);
+        }
+
+        return sb.toString();
     }
 }
