07bf49da8dea748b01fc89d43db31d93396e39e2
Yu Shu <ys2530@nyu.edu>
2016-07-19 17:03:25 -0400

    [31. Next Permutation][Compile Error]committed by Anthony

--- 31-Next-Permutation/solution.java
+++ 31-Next-Permutation/solution.java
@@ -1,12 +1,11 @@
 /**
  * 31. Next Permutation
+ * 2 solutions
  * 
  */
 
-
-
-// s1: brute force
-// O(n)
+// s1: brute force, two pointers
+// O(n^2), O(1)
 
 public class Solution {
     public void nextPermutation(int[] nums) {
@@ -44,6 +43,54 @@ public class Solution {
         for (int i = start, j = end; i < j; i++, j--) {
             swap(nums, i, j);
         }
+    }
+}
+
+// *s2: brute force, 2 times loop
+// O(n), O(n)
+
+public class Solution {
+    public void nextPermutation(int[] nums){
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        // find the first increase index
+        int index = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+            }            
+            break;
+        }
+        if (index == -1) {
+            reverse(nums, 0, nums.length - 1);
+        }
         
+        int index2 = index + 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] > nums[index]) {
+                index2 = i;
+            }
+            break;
+        }
+        swap(nums, index, index2);
+        reverse(nums, index2, nums.length - 1);
+        
+
+    }
+    
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public void reverse(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        for (int i = start, j = end; i < j; i++, j--) {
+            swap(nums, i, j);
+        }
     }
 }
