674c6120293c5d62a1811492a93102856fa8e4c3
Yu Shu <ys2530@nyu.edu>
2016-07-21 16:48:46 -0400

    [122. Best Time to Buy and Sell Stock II][Accepted]committed by Anthony

--- 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.java
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.java
@@ -57,6 +57,7 @@ public class Solution {
 // *s2: one pass 
 // O(n), O(1)
 
+/*
 public class Solution {
     public int maxProfit(int[] prices) {
         // corner case
@@ -72,4 +73,33 @@ public class Solution {
         return max;        
     }
 }
+*/
 
+
+// s3: find the valleys and peaks
+// O(n), O(1)
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        // corner case
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int valley = prices[0];
+        int peak = prices[0];
+        int i = 0;
+        int max = 0;
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1]) {
+                i++;
+            }
+            valley = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1]) {
+                i++;
+            }
+            peak = prices[i];
+            max += peak - valley;
+        }
+        return max;        
+    }
+}
