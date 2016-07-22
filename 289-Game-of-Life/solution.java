28803ece3e1c4abb0a6da69f3094a118987dd374
Yu Shu <ys2530@nyu.edu>
2016-07-22 16:31:26 -0400

    [289. Game of Life][Compile Error]committed by Anthony

--- 289-Game-of-Life/solution.java
+++ 289-Game-of-Life/solution.java
@@ -86,9 +86,11 @@ public class Solution {
 */
 
 
-// s2: To do it in place
-//
+// s2: To do it in place, use the bit operation, the higher bit stores the next generation
+// O(m * n), O(1)
+// tricky
 
+/*
 public class Solution {
     public void gameOfLife(int[][] board) {
         // corner case
@@ -160,4 +162,58 @@ public class Solution {
         return state;        
     }
 }
+*/
+
+
+// *s2: simplied s2, use bit operation
+// Because we need to solve the problem in place, we can use the higher bit to record the next state. And at the end, shift right a bit to get the next state for each cell.
+
+
+public class Solution {
+    public void gameOfLife(int[][] board) {
+        // corner case
+        if (board == null || board.length == 0) {
+            return;
+        }
+        if (board[0] == null || board[0].length == 0) {
+            return;
+        }
+        int m = board.length;
+        int n = board[0].length;
         
+        int[] xArray = {0, -1, -1, -1, 0, 1, 1, 1};
+        int[] yArray = {-1, -1, 0, 1, 1, 1, 0, -1};
+        
+        for(int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int count = 0;
+                for (int k = 0; k < 8, k++) {
+                    int x = i + xArray[k];
+                    int y = j + yArray[k];
+                    if (x > 0 && y > 0 && x < m && y < n && (board[x][y] & 1) == 1 ) {
+                        count += 1;
+                    }
+                }
+                if (count < 2 && board[i][j] == 1) {
+                    board[i][j] &= 1;
+                }
+                if ((count == 2 || count == 3) && board[i][j] == 1) {
+                    board[i][j] |= 2;
+                }
+                if (count >3 && board[i][j] == 1) {
+                    board[i][j] &= 1;
+                }
+                if (count == 3 && board[i][j] == 0) {
+                    board[i][j] |= 2;
+                }
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = board[i][j] >> 1;
+            }
+        }
+        
+    }
+}
