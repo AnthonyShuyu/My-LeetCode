ac33efef818fab53d2f0ea5b41bc4d1f06595e15
Yu Shu <ys2530@nyu.edu>
2016-07-22 17:26:57 -0400

    [80. Remove Duplicates from Sorted Array II][Compile Error]committed by Anthony

--- 80-Remove-Duplicates-from-Sorted-Array-II/solution.java
+++ 80-Remove-Duplicates-from-Sorted-Array-II/solution.java
@@ -5,54 +5,89 @@
  */
 
 
-// s1: use index
-// O(n), O(1)
+// s1: use index, and two pointers
+// O(n ^ 2), O(1)
 
 public class Solution {
     public int removeDuplicates(int[] nums) {
         // corner case
-        // if (nums == null || nums.length == 0) {
-        //     return 0;
-        // }
-        // int index = 0;
-        // int j = 0;
-        // int count = 0;
-        // for (int i = 0; i < nums.length; i++) {
-        //     if (nums[i] != nums[index]) {
-        //         if (i - j >= 2) {
-        //             nums[index + 1] = nums[j];
-        //             index += 2;
-        //             nums[index] = nums[i];
-        //             j = i;
-        //             count += 2;
-        //         } else {
-        //             index++;
-        //             nums[index] = nums[i];
-        //             j = i;
-        //             count += 1;
-        //         }
-        //     }  
-        // }
-        
-        
-        
-                if(nums == null)
+        if (nums == null || nums.length == 0) {
             return 0;
-        int cur = 0;
-        int i ,j;
-        for(i = 0; i < nums.length;){
-            int now = nums[i];
-            for( j = i; j < nums.length; j++){
-                if(nums[j] != now)
+        }
+        int index = 0;
+        int i = 0;
+        while (i < nums.length) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] != nums[i]) {
                     break;
-                if(j-i < 2)
-                    nums[cur++] = now; 
+                } else if (j - i <= 2) {
+                    nums[index] = nums[i];
+                    index++;
+                }
             }
             i = j;
         }
-        return cur;
+        
+        return index;
+        
     }
+}
 
+
+
+
+
+
+
+
+
+
+
+
+// s1: use index
+// O(n), O(1)
+// failed, can not handle the last index when return 
+
+/*
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        //corner case
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int index = 0;
+        int j = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[index]) {
+                if (i - j >= 2) {
+                    nums[index + 1] = nums[j];
+                    index += 2;
+                    nums[index] = nums[i];
+                    j = i;
+                    count += 2;
+                } else {
+                    index++;
+                    nums[index] = nums[i];
+                    j = i;
+                    count += 1;
+                }
+            }  
+        }
     }
+}
+*/
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
