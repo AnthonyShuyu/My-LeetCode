8e9ffd207d25ba99d72e718c7234fc442eee8081
Yu Shu <ys2530@nyu.edu>
2016-07-26 17:49:14 -0400

    [54. Spiral Matrix][Compile Error]committed by Anthony

--- 54-Spiral-Matrix/solution.java
+++ 54-Spiral-Matrix/solution.java
@@ -3,9 +3,60 @@
  * 
  */
 
-// s1: brute force, one loop after one loop
+// s1: brute force, one circle after one circle, but stop when only one row or one column left
 // O(n^2), O(n)
 
+public class Solution {
+    public List<Integer> spriralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<Integer>();
+        // corner case
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return result;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int min = Math.min(m, n);
+        int circle = (min + 1) / 2;
+        for (int k = 0; k < circle; k++) {
+            for (int j = k; j < n - k; j++) {
+                result.add(matrix[k][j]);
+            }
+            for (int i = k + 1; i < m - k; i++) {
+                result.add(matrix[i][m - 1 - k]);
+            }
+            // if one row or one column left, break
+            if (m - k * 2 == 1 || n - k * 2 == 1) {
+                break;
+            }
+            for (int j = m - 1 - k; j >= 0; j--) {
+                result.add(matrix[m - 1 - k][j]);
+            }
+            for (int i = m - 2; i >= 1; i--) {
+                result.add(matrix[i][k]);
+            }
+        }
+        return result;    
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// s1: brute force, one loop after one loop
+// O(n^2), O(n)
+// failed
+
+/*
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<Integer>();
@@ -21,10 +72,23 @@ public class Solution {
         int n = matrix[0].length;
         
         int min = Math.min(m, n);
-        int circle = min / 2;
-        if (circle == 0) {
-            result.add(matrix[0]);
+        int circle = (min + 1)/ 2 ;
+        // if (m == 1 && n == 1) {
+        //     result.add(matrix[0][0]);
+        // }
+        if (m == 1) {
+            for (int i = 0; i < n; i++) {
+                result.add(matrix[0][i]);
+            }
+            return result;
         }
+        if (n == 1) {
+            for (int i = 0; i < m; i++) {
+                result.add(matrix[i][0]);
+            }
+            return result;
+        }
+        
         
         for (int k = 0; k < circle; k++) {
             for (int j = k; j < n - 1 - k; j++) {
@@ -44,3 +108,4 @@ public class Solution {
         
     }
 }
+*/
