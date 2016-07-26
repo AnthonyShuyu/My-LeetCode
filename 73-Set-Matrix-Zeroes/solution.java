19860de7ad85594184830f58caef141812849eff
Yu Shu <ys2530@nyu.edu>
2016-07-26 15:44:02 -0400

    [73. Set Matrix Zeroes][Accepted]committed by Anthony

--- 73-Set-Matrix-Zeroes/solution.java
+++ 73-Set-Matrix-Zeroes/solution.java
@@ -22,7 +22,7 @@ public class Solution {
         Set<Integer> columnSet = new HashSet<Integer>();
         
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
                     rowSet.add(i);
                     columnSet.add(j);
