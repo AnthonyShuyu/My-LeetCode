7628af0f78f26a5bc41ddcf7761b353bac52c446
Yu Shu <ys2530@nyu.edu>
2016-07-27 19:58:26 -0400

    [48. Rotate Image][Accepted]committed by Anthony

--- 48-Rotate-Image/solution.java
+++ 48-Rotate-Image/solution.java
@@ -1,12 +1,14 @@
 /**
  * 48. Rotate Image
- * You are given an n x n 2D matrix representing an image.
+ * 2 solutions
  * 
+ * You are given an n x n 2D matrix representing an image.
  */
 
 // s1: brute force, find the rule
 // O(n^2), O(n^2)
 
+/*
 public class Solution {
     public void rotate(int[][] matrix) {
         // corner case
@@ -23,7 +25,6 @@ public class Solution {
         }
         
         int[][] temp = new int[n][n];
-        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 temp[j][n - i - 1] = matrix[i][j];
@@ -34,7 +35,41 @@ public class Solution {
                 matrix[i][j] = temp[i][j];
             }
         }        
+    }
+}
+*/
+
+
+// s2: exchange the 4 sides, layer by layer
+// O(n^2), O(1)
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        // corner case
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
         
+        if (m != n) {
+            return;
+        }
         
+        int layer = (n + 1) / 2;
+        
+        for (int i = 0; i < layer; i++) {
+            for (int j = i; j < n - 1 - i; j++) {
+                int temp = matrix[i][j];  // save top
+                matrix[i][j] = matrix[n - 1 - j][i]; // put left to the top
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];  // put bottom to the left
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]; // put right to the bottom
+                matrix[j][n - 1 - i] = temp;
+            }
+        }
+        return;
     }
 }
