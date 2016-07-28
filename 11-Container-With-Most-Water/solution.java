d5bb7a05b4c53339f6f7e2e970ef742ec4a872cd
Yu Shu <ys2530@nyu.edu>
2016-07-28 15:55:25 -0400

    [11. Container With Most Water][Time Limit Exceeded]committed by Anthony

--- 11-Container-With-Most-Water/solution.java
+++ 11-Container-With-Most-Water/solution.java
@@ -2,8 +2,6 @@
  * 
  * 11. Container With Most Water
  * 
- * 
- * 
  * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
  * 
  */
@@ -12,7 +10,6 @@
 // s1: brute force, 
 // O(n^2), O(1)
 // time limit exceeded
-
 
 /*
 public class Solution {
@@ -45,7 +42,9 @@ public class Solution {
 
 // s2: two pointers
 // O(n), O(1)
+// failed
 
+/*
 public class Solution{
     public int maxArea(int[] height) {
         // corner case
@@ -81,5 +80,50 @@ public class Solution{
             j = l;
         }
         return result;
+    }
+}
+
+*/
+
+// s2: two pointers
+// 
+
+public class Solution{
+    public int maxArea(int[] height) {
+        // corner case
+        if (height == null || height.length <= 1) {
+            return -1;
+        }
+        int n = height.length;
+        int result = 0;
+        int max = 0;
+        
+        for (int i = 0; i < n - 1; i++) {
+            max = findMax(height, i);
+            result = Math.max(result, max);
+        }
+        return result;
+    }
+    
+    public int findMax(int[] height, int i) {
+        // find the first one bigger than height[i]
+        int j = height.length - 1;
+        int h = height[i];
+        while (j > i) {
+            if (height[j] >= h) {
+                break;
+            } else {
+                j--;
+            }
+        }
+        if (j == height.length - 1) {
+            return (j - i) * height[i];
+        }
+        int max_value = (j - i) * height[i];
+        j++;
+        for ( ; j < height.length; j++) {
+            max_value = Math.max(max_value, (j - i) * height[j]);
+        }
+        return max_value;
     }
 }
