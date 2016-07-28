23403f9da1f5ece895b5243e19ec8f853622f281
Yu Shu <ys2530@nyu.edu>
2016-07-27 20:21:34 -0400

    [11. Container With Most Water][Compile Error]committed by Anthony

---
+++ 11-Container-With-Most-Water/solution.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * 11. Container With Most Water
+ * 
+ * 
+ * 
+ * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+ * 
+ */
+
+
+// s1: brute force, 
+// O(n^2), O(1)
+
+public class Solution {
+    public int maxArea(int[] height) {
+        // corner case
+        if (height == null || height.length == 0) {
+            return -1;
+        }
+        int n = height.length;
+        
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int lowest_height = findLowest(height, i, j);
+                max = Math.max(lowest_height, top * j - i);
+            }
+        }
+        return max;
+    } 
+    
+    public int findLowest(int[] height, int i, int j) {
+        int result = height[i];
+        for (int k = i + 1; k <= j; k++) {
+            result = Math.min(height[k], result);
+        }
+        return result;
+    }
+}
