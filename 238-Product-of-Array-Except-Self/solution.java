16f7e7f6bee8ca2a60f833df4562839e08cecd51
Yu Shu <ys2530@nyu.edu>
2016-07-29 13:16:28 -0400

    [238. Product of Array Except Self][Time Limit Exceeded]committed by Anthony

--- 238-Product-of-Array-Except-Self/solution.java
+++ 238-Product-of-Array-Except-Self/solution.java
@@ -4,11 +4,56 @@
  *
  */
 
+// s1: brute force
+// O(n^2), O(n)
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // corner case
+        if (nums == null || nums.length == 0) {
+            return nums;
+        }
+        int[] output = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            output[i] = getProduct(nums, i);
+        }
+        return output;
+    }
+    
+    public int getProduct(int[] nums, int index) {
+        int product = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == index) {
+                continue;
+            } else {
+                product *= nums[i];
+            }
+        }
+        return product;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // s1: brute force, output[i] equals the products of all the elements divide the nums[i]
 // O(n), O(n)
 // use division
 
+/*
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
         // corner case
@@ -18,11 +63,19 @@ public class Solution {
         int[] output = new int[nums.length];
         int product = 1;
         for (int i = 0; i < nums.length; i++) {
-            product *= nums[i];
+            if (nums[i] == 0) {
+                product = 0;
+                break;
+            }
         }
         for (int i = 0; i < nums.length; i++) {
-            output[i] = product / nums[i];
+            if (nums[i] == 0) {
+                output[i] = 0;
+            } else {
+                output[i] = product / nums[i];
+            }
         }
         return output;
     }
 }
+*/
