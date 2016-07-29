d1ed94c5e3d641d8da481a881e01de4fd8f486f9
Yu Shu <ys2530@nyu.edu>
2016-07-29 14:53:10 -0400

    [228. Summary Ranges][Compile Error]committed by Anthony

--- 228-Summary-Ranges/solution.java
+++ 228-Summary-Ranges/solution.java
@@ -6,7 +6,7 @@
 
 
 
-// s1: brute force
+// s1: brute force, while loop
 // O(n), O(n)
 
 public class Solution {
@@ -41,3 +41,47 @@ public class Solution {
         return result;
     }
 }
+
+
+
+// s2: brute force, for loop
+// O(n), O(n)
+
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        // corner case
+        List<String> result = new ArrayList<String>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }        
+        
+        int index = 0
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] + 1 != nums[i + 1]) {
+                if (index == i) {
+                    result.add(String.valueOf(nums[index]);
+                } else {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(nums[index]);
+                    sb.append("->");
+                    sb.append(nums[i]);
+                    result.add(sb.toString());
+                }
+                index = i + 1;
+            }
+        }
+        if (index == 0 || index == nums.length - 1) {
+            result.add(String.valueOf(nums[index]);
+        } else {
+            StringBuilder sb = new StringBuilder();
+            sb.append(nums[index]);
+            sb.append("->");
+            sb.append(nums[nums.length - 1]);
+            result.add(sb.toString());
+        }
+        return result;
+    }
+}
+
+
+
