aa4a6a7295c476f575807e1a4af6a552d9c518d6
Yu Shu <ys2530@nyu.edu>
2016-07-29 14:53:23 -0400

    [228. Summary Ranges][Compile Error]committed by Anthony

--- 228-Summary-Ranges/solution.java
+++ 228-Summary-Ranges/solution.java
@@ -55,7 +55,7 @@ public class Solution {
             return result;
         }        
         
-        int index = 0
+        int index = 0;
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] + 1 != nums[i + 1]) {
                 if (index == i) {
