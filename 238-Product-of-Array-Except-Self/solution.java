8c51ded059c253be51d217308e7675eb087fdd85
Yu Shu <ys2530@nyu.edu>
2016-07-29 13:26:18 -0400

    [238. Product of Array Except Self][Accepted]committed by Anthony

--- 238-Product-of-Array-Except-Self/solution.java
+++ 238-Product-of-Array-Except-Self/solution.java
@@ -6,7 +6,9 @@
 
 // s1: brute force
 // O(n^2), O(n)
+// time limit exceeded
 
+/*
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
         // corner case
@@ -32,28 +34,13 @@ public class Solution {
         return product;
     }
 }
+*/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// s1: brute force, output[i] equals the products of all the elements divide the nums[i]
+// s2: brute force, output[i] equals the products of all the elements divide the nums[i]
 // O(n), O(n)
-// use division
+// use division, handle the problem of 0
 
-/*
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
         // corner case
@@ -62,20 +49,38 @@ public class Solution {
         }
         int[] output = new int[nums.length];
         int product = 1;
+        int index = -1;
+        
+        // find 0
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
-                product = 0;
+                index = i;
                 break;
             }
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                output[i] = 0;
-            } else {
+        
+        if (index != -1) {
+            for (int i = 0; i < nums.length; i++) {
+                if (i == index) {
+                    continue;
+                } else if (i != index && nums[i] == 0) {
+                    product = 0;
+                    break;
+                } else {
+                    product *= nums[i];
+                }
+            }
+            output[index] = product;
+            return output;
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                product *= nums[i];
+            }
+            for (int i = 0; i < nums.length; i++) {
                 output[i] = product / nums[i];
             }
+            return output;
         }
-        return output;
+            
     }
 }
-*/
