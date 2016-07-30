b8da9eb5750bb159688f1264dafc560e471f8243
Yu Shu <ys2530@nyu.edu>
2016-07-30 12:00:57 -0400

    [8. String to Integer (atoi)][Runtime Error]committed by Anthony

--- 8-String-to-Integer-(atoi)/solution.java
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -17,7 +17,33 @@ public class Solution {
         if (str == null || str.length() == 0) {
             return 0;
         }
-        int result = Integer.valueOf(str);
+        int result = 0;
+        if (str.length() == 1) {
+            int num = str.charAt(0);
+            if (num >= 48 && num <= 57) {
+                return num;
+            } else {
+                return 0;
+            }
+        }
+        // rule out the inital not digit or '-'
+        int initial = str.charAt(0);
+        if (initial != 45 && (initial < 48 || initial > 57)) {
+            return 0;
+        }
+        
+        for (int i = 1; i < str.length(); i++) {
+            int num = str.charAt(i);
+            if (num < 48 || num > 57) {
+                return 0;
+            }            
+        }
+        
+        if (initial == 45) {
+            result = Integer.valueOf(str.substring(1, str.length())) * (-1);
+        } else {
+            result = Integer.valueOf(str);
+        }
         return result;
     }
 }
