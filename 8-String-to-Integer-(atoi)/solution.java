3104c150e2bc60213228182604bc7ba1025d3e41
Yu Shu <ys2530@nyu.edu>
2016-07-30 15:50:33 -0400

    [8. String to Integer (atoi)][Runtime Error]committed by Anthony

--- 8-String-to-Integer-(atoi)/solution.java
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -19,43 +19,90 @@ public class Solution {
         }
         int result = 0;
         long temp = 0;
+        
+        int index = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                index++;
+            } else {
+                break;
+            }
+        }
+        str = str.substring(index, str.length());
+        
+        if (str.length() == 0) {
+            return 0;
+        }
+        
+        // rule out the str length equals to 1
         if (str.length() == 1) {
             int num = str.charAt(0);
             if (num >= 48 && num <= 57) {
-                return num;
+                return Integer.valueOf(str);
             } else {
                 return 0;
             }
         }
-        // rule out the inital not digit or '-'
+        
+        
+        
+        // rule out the initial not digit or '-' or '+'
         int initial = str.charAt(0);
-        if (initial != 45 && (initial < 48 || initial > 57)) {
+        if (initial != 45 && (initial < 48 || initial > 57) && initial != 43) {
             return 0;
         }
         
-        for (int i = 1; i < str.length(); i++) {
-            int num = str.charAt(i);
-            if (num < 48 || num > 57) {
-                return 0;
-            }            
+        
+        // // rule out the index from 1 to str.length() - 1 not equals to digit
+        // for (int i = 1; i < str.length(); i++) {
+        //     int num = str.charAt(i);
+        //     if (num < 48 || num > 57) {
+        //         return 0;
+        //     }            
+        // }
+        
+        if (str.length() > 11) {
+            return 0;
         }
         
         // rule out the string > 2^31 - 1 which is 2147483647 or string < -2^31 which is -2147483648
         
         if (initial == 45) {
-            temp = (Long.valueOf(str.substring(1, str.length())) * (-1));
+            int nonDigit = findNonDigit(str, 1);
+            String tempStr = str.substring(1, nonDigit);
+            temp = Long.valueOf(tempStr) * (-1);
             if (temp < -2147483648) {
                 return 0;
             } else {
                 return (int)temp;
             }
+        } else if (initial == 43) {
+            int nonDigit = findNonDigit(str, 1);
+            String tempStr = str.substring(1, nonDigit);
+            temp = Long.valueOf(tempStr);
+            if (temp > 2147483647) {
+                return 0;
+            } else {
+                return (int)temp;
+            }
         } else {
-            temp = Long.valueOf(str);
+            int nonDigit = findNonDigit(str, 0);
+            String tempStr = str.substring(0, nonDigit);
+            temp = Long.valueOf(tempStr);
             if (temp > 2147483647) {
                 return 0;
             } else {
                 return (int)temp;
             }
         }
+    }
+    
+    public int findNonDigit(String s, int index) {
+        for (int i = index; i < s.length(); i++) {
+            if (s.charAt(i) < 48 || s.charAt(i) > 57) {
+                return i;
+            }
+        }
+        return s.length();
     }
 }
