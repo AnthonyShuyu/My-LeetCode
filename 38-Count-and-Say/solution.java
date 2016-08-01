3354d1ee4e9a0b4be382c00a87f7e686ca0a7c62
Yu Shu <ys2530@nyu.edu>
2016-08-01 19:44:47 -0400

    [38. Count and Say][Runtime Error]committed by Anthony

---
+++ 38-Count-and-Say/solution.java
@@ -0,0 +1,44 @@
+/**
+ * 38. Count and Say
+ * 
+ * 
+ * The count-and-say sequence is the sequence of integers beginning as follows:
+1, 11, 21, 1211, 111221, ...
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth sequence.
+ * 
+ */
+
+public class Solution {
+    public String countAndSay(int n) {
+        // corner case
+        if (n <= 0) {
+            return null;
+        }
+        String s = "1";
+        for (int i = 0; i < n; i++) {
+            s = generateNextString(s);
+        }
+        return s;
+    }
+    
+    public String generateNextString(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count;
+        int index = 0;
+        while (index < s.length()) {
+            count = 0;
+            int num = s.charAt(index) - '0';
+            while ((s.charAt(index) - '0') == num) {
+                index++;
+                count++;
+            }
+            sb.append(count);
+            sb.append(num);
+        }
+        return sb.toString();
+    }
+}
