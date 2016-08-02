ca355fca437db7738a648f73773ae6598a566698
Yu Shu <ys2530@nyu.edu>
2016-08-02 17:00:01 -0400

    [224. Basic Calculator][Compile Error]committed by Anthony

--- 224-Basic-Calculator/solution.java
+++ 224-Basic-Calculator/solution.java
@@ -6,8 +6,9 @@
 
 // s1: use stack, and the num * 10 + current_num
 // O(n), O(n)
+// failed, can not use the same exactly method of Basic Calculator II
 
-
+/*
 public class Solution {
     public int calculate(String s) {
         // corner case
@@ -17,6 +18,7 @@ public class Solution {
         s = s.trim();
         Stack<Integer> stack = new Stack<Integer>();
         char sign = '+';
+        char sign2 = '+';
         int num = 0;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
@@ -25,14 +27,16 @@ public class Solution {
             }
             if (c == '(') {
                 stack.push(null);
+                sign2 = sign;
+                sign = '+';
             }
             if (Character.isDigit(c)) {
                 num = num * 10 + (int)(c - '0');
             }
-            if (c == '+' || c == '-' || (i == s.length() - 1) && Character.isDigit(c)) {
-                if (c == '+') {
+            if (c == '+' || c == '-' || c == ')' || (i == s.length() - 1) && Character.isDigit(c)) {
+                if (sign == '+') {                  // use sign, not c
                     stack.push(num);
-                } else if (c == '-') {
+                } else if (sign == '-') {
                     stack.push(-num);
                 }
                 sign = c;
@@ -40,18 +44,81 @@ public class Solution {
             } 
             if (c == ')') {
                 int temp = 0;
-                while(stack.peak() != null) {
+                while(stack.peek() != null) {
                     temp = temp + stack.pop();
                 }
                 stack.pop();
-                stack.push(temp);
+                if (sign2 == '-') {
+                    stack.push(-temp);
+                } else {
+                    stack.push(temp);
+                }
             }
         }
         int result = 0;
         for (int i : stack) {
             result += i;
         }
-        
         return result;
+    }
+}
+*/
+
+// s2: use 2 stacks, one store digits, one store signs
+
+// This problem can be solved by using a stack. We keep pushing element to the stack, when ')" is met, calculate the expression up to the first "(".
+    
+// O(n), O(n)
+
+public class Solution {
+    public int calculate(String s) {
+        // corner case
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        s = s.trim();
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Character> stack2 = new Stack<Character>();
+        int num = 0;
+        stack2.push('+');
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                continue;
+            }
+            if (c == '(') {
+                stack1.push(null);
+            }
+            if (Character.isDigit(c)) {
+                num = num * 10 + (int)(c - '0');
+            }
+            if (c == '+' || c == '-' || c == ')' || i == s.length() - 1) {
+                if (!stack2.isEmpty()) {
+                    char tempChar = stack2.pop();
+                    if (tempChar == '+') {
+                        stack1.push(num);
+                    }  else {
+                        stack1.push(-num);
+                    }
+                }
+                if (c == '+') {
+                    stack2.push('+');
+                } else if (c == '-') {
+                    stack2.push('-');
+                } else if (c == ')') {
+                    int temp = 0;
+                    while (!stack1.isEmpty() && stack1.peek() != null) {
+                        temp += stack1.pop();
+                    }
+                    stack1.pop();
+                    stack1.push(temp);
+                }
+            }
+            int result = 0;
+            for (int i : stack1) {
+                result += i;
+            }
+            return result;
+        }
     }
 }
