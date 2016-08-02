141863d76283889c60f229e8339c636502f24960
Yu Shu <ys2530@nyu.edu>
2016-08-02 14:47:09 -0400

    [227. Basic Calculator II][Accepted]committed by Anthony

--- 227-Basic-Calculator-II/solution.java
+++ 227-Basic-Calculator-II/solution.java
@@ -18,16 +18,16 @@ public class Solution {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        // s = s.trim();
+        s = s.trim();
         Stack<Integer> stack = new Stack<Integer>();
         int num = 0;
         int result = 0;
         char sign = '+';
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            // if (s.charAt(i) == ' ') {
-            //     continue;
-            // } 
+            if (c == ' ') {
+                continue;
+            } 
             if (Character.isDigit(c)) {
                 int current_num = (int)(s.charAt(i) - '0');
                 num = num * 10 + current_num;
