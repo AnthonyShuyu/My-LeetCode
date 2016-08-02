49a20b1a79dd57c0d663481daf83e7a35cab0e5d
Yu Shu <ys2530@nyu.edu>
2016-08-01 23:15:33 -0400

    [58. Length of Last Word][Compile Error]committed by Anthony

---
+++ 58-Length-of-Last-Word/solution.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * 58. Length of Last Word
+ * 
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+If the last word does not exist, return 0.
+ * 
+ */
+
+// s1: brute force
+// O(n), O(1)
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        // corner case
+        if (s == null || s.length() == 0) {
+            return 0;
+        } 
+        int count = 0;
+        int index = s.length() - 1;
+        while (index >= 0) {
+            if (Character.isLetter(s.charAt(index))) {
+                break;
+            }
+            index--;
+        }
+        if (index < 0) {
+            return 0;
+        }
+        while (index >= 0) {
+            if (Character.isLetter(s.charAt(index)) {
+                count++;
+                index--;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+}
