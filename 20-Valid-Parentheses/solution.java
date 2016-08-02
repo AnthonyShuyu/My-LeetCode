184bf35a82747e555dd127de2279e964668f710f
Yu Shu <ys2530@nyu.edu>
2016-08-01 22:14:10 -0400

    [20. Valid Parentheses][Compile Error]committed by Anthony

---
+++ 20-Valid-Parentheses/solution.java
@@ -0,0 +1,56 @@
+/**
+ * 20. Valid Parentheses
+ * 
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ */
+
+
+// s1: use stack
+// O(n), O(n)
+
+public class Solution {
+    public boolean isValid(String s) {
+        // corner case
+        if (s == null || s.length() <= 1) {
+            return false;
+        }
+        Stack stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    if (stack.pop() != '(') {
+                        return false;
+                    }
+                }
+            } else if (s.charAt(i) == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    if (stack.pop() != ']') {
+                        return false;
+                    }
+                }
+            } else if (s.charAt(i) == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    if (stack.pop() != '}') {
+                        return false;
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+}
