e68abf6363280b19c5fc21467cc6b3b6fbc93856
Yu Shu <ys2530@nyu.edu>
2016-08-02 19:46:48 -0400

    [71. Simplify Path][Compile Error]committed by Anthony

---
+++ 71-Simplify-Path/solution.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ * 71. Simplify Path
+ * 
+ * 
+ */
+
+
+public class Solution {
+    public String simplifyPath(String path) {
+        // corner case
+        if (path == null || path.length() == 0) {
+            return path;
+        }
+        
+        path = path.repalce("//", "/");
+        if (path.equals("/../")) {
+            return "/";
+        }
+        List<String> arrayList = new ArrayList<String>();
+        int index = path.length() - 1;
+        while (index >=  0) {
+            if (path.charAt(index) == '.') {
+                break;
+            }
+            if (Character.isDigit(path.charAt(index))) {
+                int j = index - 1;
+                while (j >= 0) {
+                    if (Character.isDigit(path.charAt(j))) {
+                        j--;
+                    } else {
+                        break;
+                    }
+                }
+                if (j >= 0) {
+                    String subpath = path.substring(j, index + 1);
+                    subpath += "/";
+                    arrayList.add(subpath);
+                }
+                index = j;
+            }
+            index--;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = arrayList.size(); i >= 0; i--) {
+            sb.append(arrayList.get(i));
+        }
+        return sb.toString();
+    }
+}
