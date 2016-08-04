cd5f704170a15e6d23e6f5d00bf92cc1689954d9
Yu Shu <ys2530@nyu.edu>
2016-08-04 17:19:53 -0400

    [36. Valid Sudoku][Accepted]committed by Anthony

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -7,6 +7,7 @@
 // s1: brute force
 // O(n^2), O(n^2)
 
+/*
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
         if (board == null || board.length != 9) {
@@ -56,6 +57,81 @@ public class Solution {
         }
         return true;
     }
+    
+    public boolean validNums(char[] nums) {
+        if (nums == null || nums.length != 9) {
+            return false;
+        }
+        Set<Character> hashSet = new HashSet<Character>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == '.') {
+                continue;
+            }
+            if (nums[i] > '9' || nums[i] < '1') {
+                return false;
+            } else if (hashSet.contains(nums[i])){
+                return false;
+            } else {
+                hashSet.add(nums[i]);
+            }
+        }
+        return true;
+    }
+}
+*/
+
+
+// *s2: brute force
+// O(n^2), O(n)
+
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // corner case
+        if (board == null || board.length != 9) {
+            return false;
+        }
+        if (board[0] == null || board[0].length != 9) {
+            return false;
+        }
+        // rows
+        for (int i = 0; i < 9; i++) {
+            char[] chars = new char[9];
+            for (int j = 0; j < 9; j++) {
+                chars[j] = board[i][j]; 
+            }
+            if (!validNums(chars)) {
+                return false;
+            }
+        }
+                
+        // cols
+        for (int i = 0; i < 9; i++) {
+            char[] chars = new char[9];
+            for (int j = 0; j < 9; j++) {
+                chars[j] = board[j][i];
+            }
+            if (!validNums(chars)) {
+                return false;
+            }
+        }
+        
+        // boxes
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                char[] chars = new char[9];
+                for (int k = 0; k < 9; k++) {
+                    chars[k] = board[i + k / 3][j + k % 3];
+                }
+                if (!validNums(chars)) {
+                    return false;
+                }               
+            }
+        }
+        return true;
+    }
+    
+    
     
     public boolean validNums(char[] nums) {
         if (nums == null || nums.length != 9) {
