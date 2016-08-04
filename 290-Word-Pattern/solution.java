182e903d68ae744b090df69978be8277e4d343bd
Yu Shu <ys2530@nyu.edu>
2016-08-04 15:55:12 -0400

    [290. Word Pattern][Compile Error]committed by Anthony

---
+++ 290-Word-Pattern/solution.java
@@ -0,0 +1,35 @@
+/**
+ * 290. Word Pattern
+ * 
+ * 
+ */
+
+// s1: HashMap, Isomorphic Strings
+// O(n), O(n)
+
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        // corner case
+        if (pattern == null || str == null) {
+            return false;
+        }
+        String[] strs = str.split(' ');
+        if (strs.length != pattern.length()) {
+            return false;
+        }
+        Map<Character, String> hashMap = new HashMap<Character, String>();
+        for (int i = 0; i < pattern.length(); i++) {
+            if (hashMap.containsKey(pattern.charAt(i))) {
+                if (!hashMap.get(pattern.charAt(i)).equals(strs[i])) {
+                    return false;
+                }
+            } else {
+                if (hashMap.containsValue(strs[i])) {
+                    return false;
+                }
+                hashMap.put(pattern.charAt(i), strs[i]);
+            }
+        } 
+        return true;
+    }
+}
