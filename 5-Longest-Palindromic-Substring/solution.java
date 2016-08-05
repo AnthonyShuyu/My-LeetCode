59ac7ce4e6d03af21f9d66d6bb22215fd477898f
Yu Shu <ys2530@nyu.edu>
2016-08-05 15:10:53 -0400

    [5. Longest Palindromic Substring][Compile Error]committed by Anthony

---
+++ 5-Longest-Palindromic-Substring/solution.java
@@ -0,0 +1,48 @@
+/**
+ * 5. Longest Palindromic Substring
+ * 
+ * 
+ */
+
+// s1: two pointers, start <= end
+// O(n^2), O(1)
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        String result = s.charAt(0);
+        int max_length = 1;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = s.length; j > i; j--) {
+                String substring = s.substring(i, j);
+                if (isPalindrome(substring)) {
+                    if (substring.length() > max_length) {
+                        max_length = substring.length();
+                        result = substring;
+                    }
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+    
+    
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return false;
+        }
+        int start = 0;
+        int end = s.length() - 1;
+        for (int i = start, j = end; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    
+}
