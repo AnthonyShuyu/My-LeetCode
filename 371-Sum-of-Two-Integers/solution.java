06248f1aed9ea2510a87ae6ab22dc6cad3aefa22
Yu Shu <ys2530@nyu.edu>
2016-08-05 20:28:34 -0400

    [371. Sum of Two Integers][Accepted]committed by Anthony

--- 371-Sum-of-Two-Integers/solution.java
+++ 371-Sum-of-Two-Integers/solution.java
@@ -1,5 +1,10 @@
 public class Solution {
     public int getSum(int a, int b) {
-        return a + b;
+        while (b != 0) {
+            int c = a & b;
+            a = a ^ b;
+            b = c << 1;
+        }
+        return a;
     }
 }
