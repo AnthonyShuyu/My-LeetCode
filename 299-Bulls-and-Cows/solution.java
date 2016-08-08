f54209e1233a8879edf1f9f813fbc5f9dce38cbe
Yu Shu <ys2530@nyu.edu>
2016-08-07 23:50:03 -0400

    [299. Bulls and Cows][Compile Error]committed by Anthony

--- 299-Bulls-and-Cows/solution.java
+++ 299-Bulls-and-Cows/solution.java
@@ -23,32 +23,62 @@ public class Solution {
             return result;
         }
         int countA = 0;
-        int countB = 0;
+        int count = 0;
         
-        // use hashMap to store index and value
-        Map<Integer, Character> hashMap = new HashMap<Integer, Character>();
-        
-        // use hashSet to store the different cows
-        Set<Character> hashSet = new HashSet<Character>();
-        
+        // countA
         for (int i = 0; i < secret.length(); i++) {
-            hashMap.put(i, secret.charAt(i));
+            if (secret.charAt(i) == guess.charAt(i)) {
+                countA++;
+            }
+        }
+        
+        
+        
+        
+        // countA + countB, use the HashMap to store the value and count of the number
+        Map<Character, Integer> hashMap = new HashMap<Character, Integer>();
+        for (int i = 0; i < secret.length(); i++) {
+            char c = secret.charAt(i);
+            if(hashMap.containskey(c) {
+                hashMap.put(c, hashMap.get(c) + 1);
+            } else {
+                hashMap.put(c, 1);
+            }
         }
         
         for (int i = 0; i < guess.length(); i++) {
-            if (hashMap.containsKey(i)) {
-                if (hashMap.get(i) == guess.charAt(i)) {
-                    countA++;
-                } else {
-                    if (hashMap.containsValue(guess.charAt(i))) {
-                        // if (!hashSet.contains(guess.charAt(i))) {
-                            countB++;
-                            // hashSet.add(guess.charAt(i));
-                        }
-                    }
-                }
+            char c = guess.charAt(i);
+            if (hashMap.containsKey(c) && hashMap.get(c) > 0) {
+                count++;
+                hashMap.put(c, hashMap.get(c) - 1);
             }
         }
+        
+        int countB = count - countA;
+        
+        // // use hashMap to store index and value
+        // Map<Integer, Character> hashMap = new HashMap<Integer, Character>();
+        
+        // // use hashSet to store the different cows
+        // // Set<Character> hashSet = new HashSet<Character>();
+        
+        // for (int i = 0; i < secret.length(); i++) {
+        //     hashMap.put(i, secret.charAt(i));
+        // }
+        
+        // for (int i = 0; i < guess.length(); i++) {
+        //     if (hashMap.containsKey(i)) {
+        //         if (hashMap.get(i) == guess.charAt(i)) {
+        //             countA++;
+        //         } else {
+        //             if (hashMap.containsValue(guess.charAt(i))) {
+        //                 // if (!hashSet.contains(guess.charAt(i))) {
+        //                     countB++;
+        //                     // hashSet.add(guess.charAt(i));
+        //                 }
+        //             }
+        //         }
+        //     }
         
         StringBuilder sb = new StringBuilder();
         sb.append(countA + "A" + countB + "B");
