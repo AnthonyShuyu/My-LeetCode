12158f9d0947b6f1b68bf2a239034b8a42afddbd
Yu Shu <ys2530@nyu.edu>
2016-08-08 17:02:39 -0400

    [166. Fraction to Recurring Decimal][Compile Error]committed by Anthony

--- 166-Fraction-to-Recurring-Decimal/solution.java
+++ 166-Fraction-to-Recurring-Decimal/solution.java
@@ -1,5 +1,24 @@
+/**
+ * 
+ * 166. Fraction to Recurring Decimal
+ * 
+ * 
+ */ 
+
+
+
 public class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
+        // corner case
+        if (denominator == 0) {
+            return null;
+        }
+        int factor = numerator / denominator;
+        int remainder = numerator % denominator;
+        
+        if (factor > 0 && remainder == 0) {
+            return factor;
+        }
         
     }
 }
