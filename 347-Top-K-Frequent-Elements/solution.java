3df0a6e7533c6e04902100bafd23d64c751af8ae
Yu Shu <ys2530@nyu.edu>
2016-08-09 16:00:16 -0400

    [347. Top K Frequent Elements][Compile Error]committed by Anthony

--- 347-Top-K-Frequent-Elements/solution.java
+++ 347-Top-K-Frequent-Elements/solution.java
@@ -2,12 +2,15 @@
  * 347. Top K Frequent Elements
  * Given a non-empty array of integers, return the k most frequent elements.
  * 
+ * 2 solutions
+ * 
  */
 
 
-// s1: HashMap
-// O(n), O(n)
+// s1: HashMap + HashSet
+// O(k^2), O(n)
 
+/*
 public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
         // corner case
@@ -44,5 +47,54 @@ public class Solution {
             }
         }
         return index;
+    }
+}
+*/
+
+
+// s2: HashMap + priorityQueue
+// O(n), O(n)
+
+
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        // corner case
+        List<Integer> result = new ArrayList<Integer>();
+        
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        Queue<Map.Entry<Integer, Integer>> priorityQueue = new PriorityQueue<Map.Entry<Integer, Integer>>({
+            new Comparator<Map.Entry<Integer, Integer>>() {
+                public int compare(Map.Entry<Integer, Integer> e1, Map.Entry<Integer, Integer> e2) {
+                    return e1.getValue() - e2.getValue();
+                }
+            }   
+        });
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(nums[i])) {
+                hashMap.put(nums[i], hashMap.get(nums[i]) + 1);
+            } else {
+                hashMap.put(nums[i], 1);
+            }
+        }
+        
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+            if (priorityQueue.size() < k) {
+                priorityQueue.offer(entry.getKey());
+            } else {
+                if (priorityQueue.peek() < entry.getValue()) {
+                    priorityQueue.poll();
+                    priorityQueue.offer(entry.getKey());
+                }
+            }
+        }
+        
+        for (Map.Entry<Integer, Integer> entry : priorityQueue) {
+            result.add(entry);
+        }
+        return result;
     }
 }
