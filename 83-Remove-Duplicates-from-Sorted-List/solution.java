585dc12142384fef6e34886971951bef28cf2572
Yu Shu <ys2530@nyu.edu>
2016-08-10 00:06:50 -0400

    [83. Remove Duplicates from Sorted List][Runtime Error]committed by Anthony

--- 83-Remove-Duplicates-from-Sorted-List/solution.java
+++ 83-Remove-Duplicates-from-Sorted-List/solution.java
@@ -13,6 +13,12 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+ 
+ 
+// s1: brute force
+// O(n), O(1)
+
+/*
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         // corner case
@@ -29,5 +35,29 @@ public class Solution {
             head = head.next;
         }
         return node;
+    }
+}
+*/
+
+// s2: use dummy node
+// O(n), O(1)
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        // corner case
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        while (head != null) {
+            if (head.val == head.next.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return dummy.next;
     }
 }
