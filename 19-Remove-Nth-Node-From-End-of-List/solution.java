a34a2076ee0ee869f9e7fcdc58a104365ef382a2
Yu Shu <ys2530@nyu.edu>
2016-08-12 14:57:53 -0400

    [19. Remove Nth Node From End of List][Runtime Error]committed by Anthony

---
+++ 19-Remove-Nth-Node-From-End-of-List/solution.java
@@ -0,0 +1,58 @@
+/**
+ * 19. Remove Nth Node From End of List
+ * 
+ * 
+ */
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+// s1: brute force, dummy node, move (length of the list - n) steps
+// O(n), O(1)
+
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // corner case
+        if (head == null || n <= 0) {
+            return null;
+        }
+        
+        //count the length of the list
+        int length = 0;
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+        
+        int step = length - n;
+        if (step < 0) {
+            return null;
+        }
+        
+        // create dummy node
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        for (int i = 0; i < step; i++) {
+            if (head != null) {
+                head = head.next;
+            } else {
+                return null;
+            }
+        }
+        if (head.next != null) {
+            head.next = head.next.next;
+        }
+        return dummy.next;
+        
+    }
+}
