8b8be875a6ac9aaccbdcdee142bb9cc70ea92b8e
Yu Shu <ys2530@nyu.edu>
2016-08-12 16:56:15 -0400

    [86. Partition List][Accepted]committed by Anthony

--- 86-Partition-List/solution.java
+++ 86-Partition-List/solution.java
@@ -16,8 +16,10 @@
  
  
 // s1: brute force, create another listNode and merge them, use dummy node
-// O(n), O(1)
+// O(n), O(n)
  
+ 
+/* 
 public class Solution {
     public ListNode partition(ListNode head, int x) {
         // corner case
@@ -44,6 +46,42 @@ public class Solution {
             head = head.next;
         }
         node1.next = dummy2.next;
+        return dummy1.next;
+    }
+}
+
+*/
+
+
+
+// *s2: two dummy nodes, two heads, don't forget: head2.next = null
+// O(n), O(1)
+
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        // corner case
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode dummy1 = new ListNode(0);
+        ListNode dummy2 = new ListNode(0);
+        
+        ListNode head1 = dummy1;
+        ListNode head2 = dummy2;
+        
+        while (head != null) {
+            if (head.val < x) {
+                head1.next = head;
+                head1 = head1.next;
+            } else {
+                head2.next = head;
+                head2 = head2.next;
+            }
+            head = head.next;
+        }
+        head1.next = dummy2.next;
+        head2.next = null;
         return dummy1.next;
     }
 }
