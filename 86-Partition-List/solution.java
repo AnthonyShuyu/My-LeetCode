16323c5e68cf45043f7214fc6be09c514cb0f103
Yu Shu <ys2530@nyu.edu>
2016-08-13 13:29:53 -0400

    [86. Partition List][Accepted]committed by Anthony

--- 86-Partition-List/solution.java
+++ 86-Partition-List/solution.java
@@ -92,6 +92,7 @@ public class Solution {
 // s3: one dummy node, not recommended now
 // O(n), O(1)
 
+/*
 public class Solution {
     public ListNode partition(ListNode head, int x) {
         if (head == null || head.next == null) {
@@ -119,5 +120,41 @@ public class Solution {
         }
         node.next = null;
         return dummy.next;
+    }
+} 
+*/
+
+
+// practice
+// O(n), O(1)
+
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        // corner case
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode dummy1 = new ListNode(0);
+        ListNode dummy2 = new ListNode(0);
+        
+        ListNode head1 = dummy1;
+        ListNode head2 = dummy2;
+        
+        while (head != null) {
+            if (head.val < x) {
+                head1.next = head;
+                head1 = head1.next;
+            } else {
+                head2.next = head;
+                head2 = head2.next;
+            }
+            head = head.next;
+        }
+        
+        head1.next = dummy2.next;
+        head2.next = null;
+        return dummy1.next;
+        
     }
 }
