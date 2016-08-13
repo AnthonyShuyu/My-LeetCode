5fcb6e49b38d1a5e4f931c28dc196ca57a910a77
Yu Shu <ys2530@nyu.edu>
2016-08-13 15:23:01 -0400

    [206. Reverse Linked List][Accepted]committed by Anthony

---
+++ 206-Reverse-Linked-List/solution.java
@@ -0,0 +1,37 @@
+/**
+ * 206. Reverse Linked List
+ * 
+ * 
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+// s1: brure force
+//use two pointers, one is the prev, one is the head, and move to the next node together, remember the 4 steps.
+// 4 steps， the variale appears at the right of the "=" will appear at the left of the next "="
+// O(n), O(1)
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        // corner case
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
