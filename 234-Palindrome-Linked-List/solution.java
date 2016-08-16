6e5455d506544f05f04dc5a6587e09c376c55ba0
Yu Shu <ys2530@nyu.edu>
2016-08-16 15:58:01 -0400

    [234. Palindrome Linked List][Accepted]committed by Anthony

--- 234-Palindrome-Linked-List/solution.java
+++ 234-Palindrome-Linked-List/solution.java
@@ -10,6 +10,7 @@
 // s1: use an arrayList to check if it is a palindrome or not
 // O(n), O(n)
 
+/*
 public class Solution {
     public boolean isPalindrome(ListNode head) {
         // corner case
@@ -31,6 +32,51 @@ public class Solution {
             if (!arrayList.get(i).equals(arrayList.get(j))) {
                 return false;
             }
+        }
+        return true;
+    }
+}
+*/
+
+
+// s2: follow up, reverse and compare, fast and slow pointers
+// O(n), O(1)
+
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        // corner case
+        if (head == null || head.next == null) {
+            return true;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        ListNode node2 = slow.next;
+        slow.next = null;
+        ListNode node1 = head;
+        
+        // reverse node2
+        ListNode prev = null;
+        while (node2 != null) {
+            ListNode temp = node2.next;
+            node2.next = prev;
+            prev = node2;
+            node2 = temp;
+        }
+        
+        // compare the two lists
+        while (prev != null) {
+            if (node1.val != prev.val) {
+                return false;
+            }
+            prev = prev.next;
+            node1 = node1.next;
         }
         return true;
     }
