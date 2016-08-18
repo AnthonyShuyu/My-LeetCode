216f0ef93529f17ed4a8a854fe234552eafdaea2
Yu Shu <ys2530@nyu.edu>
2016-08-18 12:33:50 -0400

    [141. Linked List Cycle][Accepted]committed by Anthony

--- 141-Linked-List-Cycle/solution.java
+++ 141-Linked-List-Cycle/solution.java
@@ -50,11 +50,14 @@ public class Solution {
         } 
         ListNode slow = head;
         ListNode fast = head.next;
-        while (fast != null && slow != fast) {
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
         }
-        return fast != null;
+        return false;
     }
 }
 
