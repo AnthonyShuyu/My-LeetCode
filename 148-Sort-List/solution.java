f35dee1a536feca484045aa67a0763416615a0ee
Yu Shu <ys2530@nyu.edu>
2016-08-18 17:58:38 -0400

    [148. Sort List][Runtime Error]committed by Anthony

--- 148-Sort-List/solution.java
+++ 148-Sort-List/solution.java
@@ -82,12 +82,10 @@ public class Solution {
         
         if (head1 != null) {
             node.next = head1;
-            head1 = head1.next;
         }
         
         if (head2 != null) {
             node.next = head2;
-            head2 = head2.next;
         }
         return dummy.next;
     }
