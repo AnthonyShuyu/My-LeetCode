7384e6eb270d3718119b1565f66a8a10608aa2ff
Yu Shu <ys2530@nyu.edu>
2016-08-18 17:38:50 -0400

    [148. Sort List][Compile Error]committed by Anthony

---
+++ 148-Sort-List/solution.java
@@ -0,0 +1,86 @@
+/**
+ * 148. Sort List
+ * 
+ * 
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+ 
+// s1: O(nlogn) time and O(1) space, think of merge sort, divide and conquer(merge two linked lists)
+// O(nlogn), O(1)
+
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        int length = countLength(head);
+        ListNode node = head;
+        for (int i = 0; i < count / 2; i++) {
+            node = node.next;
+        }
+        
+        ListNode right = node.next;
+        node.next = null;
+        ListNode left = head;
+        
+        ListNode leftList = sortList(left);
+        ListNode rightList = sortList(right);
+        return mergeList(leftList, rightList);
+        
+    }
+    
+    public int countLength(ListNode head) {
+        int count = 0;
+        while (head != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+    
+    
+    
+    // LeetCode 
+    public ListNode mergeLists(ListNode head1, ListNode head2) {
+        if (head1 == null) {
+            return head2;
+        }
+        if (head2 == null) {
+            return head1;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
+        
+        while (head1 != null && head2 != null) {
+            if (head1.val <= head2.val) {
+                node.next = head1.val;
+                head1 = head1.next;
+            } else {
+                node.next = head2.val;
+                head2 = head2.next;
+            }
+            node = node.next;
+        }
+        
+        if (head1 != null) {
+            node.next = head1;
+        }
+        
+        if (head2 != null) {
+            node.next = head2;
+        }
+        return dummy.next;
+    }
+}
