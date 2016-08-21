2bace8d71495a36702e806ad54a0ff2e83e1a123
Yu Shu <ys2530@nyu.edu>
2016-08-20 22:59:17 -0400

    [138. Copy List with Random Pointer][Compile Error]committed by Anthony

---
+++ 138-Copy-List-with-Random-Pointer/solution.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ * 138. Copy List with Random Pointer
+ * 
+ */
+
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+ 
+ 
+// s1: use hahsMap to map original node to the new node
+// O(n), O(n)
+
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        // corner case
+        if (head == null) {
+            return null;
+        }
+        RandomListNode dummy = new RandomListNode(0);
+        RandomListNode node = dummy;
+        
+        Map<RandomListNode, RandomListNode> hashMap = new HashMap<RandomListNode, RandomListNode>();
+        
+        RandomListNode newNode;
+        while (head != null) {
+            if (!hashMap.containsKey(head)) {
+                newNode = new RandomListNode(head.val);
+                hashMap.put(head, newNode);
+            } else {
+                newNode = hashMap.get(head);
+            }
+            node.next = newNode;
+            
+            if (head.random != null) {
+                if (!hashMap.containsKey(head.random)) {
+                    newNode.random = new RandomListNode(head.random.val);
+                    hashMap.put(head.random, newNode.random);
+                } else {
+                    newNode.random = hashMap.get(head.random);
+                }
+            }
+            node = node.next;
+            head = head.next;
+        }
+        return dummy.next;
+    }
+}
