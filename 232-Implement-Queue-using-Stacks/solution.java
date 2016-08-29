721258095425867a1ca99678b18d9d49e1be444a
Yu Shu <ys2530@nyu.edu>
2016-08-29 13:24:33 -0400

    [232. Implement Queue using Stacks][Compile Error]committed by Anthony

---
+++ 232-Implement-Queue-using-Stacks/solution.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ * 232. Implement Queue using Stacks
+ * 
+ * 
+ */
+
+
+// s1: use 2 stacks
+// O(1), O(n)
+
+class MyQueue {
+    
+    Stack stack1 = new Stack();
+    Stack stack2 = new Stac();
+    
+    // Push element x to the back of queue.
+    public void push(int x) {
+        stack1.push(x);        
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        convert(stack1, stack2);
+        stack2.pop();
+        convert(stack2, stack1);
+    }
+
+    // Get the front element.
+    public int peek() {
+        convert(stack1, stack2);
+        int result = stack2.peek();
+        convert(stack2, stack1);
+        return result;
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+    
+    public void convert(Stack stack1, Stack stack2) {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+    }
+    
+}
