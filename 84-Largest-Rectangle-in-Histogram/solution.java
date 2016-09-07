cd730a01e794a67c41c10676099bb71ff524577b
Yu Shu <ys2530@nyu.edu>
2016-09-07 14:22:49 -0400

    [84. Largest Rectangle in Histogram][Compile Error]committed by Anthony

--- 84-Largest-Rectangle-in-Histogram/solution.java
+++ 84-Largest-Rectangle-in-Histogram/solution.java
@@ -18,7 +18,7 @@ public class Solution {
         int min_height = 0;
         for (int i = 0; i < heights.length; i++) {
             min_height = heights[i];
-            for (int j = i; j < heigths.length; j++) {
+            for (int j = i; j < heights.length; j++) {
                 min_height = Math.min(min_height, height[j]);
                 max = Math.max(max, (j - i + 1) * min_height);
             }
