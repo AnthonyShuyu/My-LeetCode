e7bf6babbf42233ea9bb804c2e14788f81fcf0cd
Yu Shu <ys2530@nyu.edu>
2016-09-08 14:35:55 -0400

    [202. Happy Number][Accepted]committed by Anthony

---
+++ 202-Happy-Number/solution.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ * 202. Happy Number
+ * 
+ */
+
+
+// s1: use a hashSet
+// O(n), O(n)
+
+public class Solution {
+    public boolean isHappy(int n) {
+        // corner case
+        if (n <= 0) {
+            return false;
+        }
+        
+        Set<Integer> hashSet = new HashSet<Integer>();
+        while (n != 1) {
+            if (hashSet.contains(n)) {
+                return false;
+            } else {
+                hashSet.add(n);
+            }
+            n = count(n);
+        }
+        return true;
+    }
+    
+    
+    public int count(int n) {
+        int result = 0;
+        while (n > 0) {
+            result += Math.pow(n % 10, 2);
+            n = n / 10;
+        }
+        return result;
+    }
+}
