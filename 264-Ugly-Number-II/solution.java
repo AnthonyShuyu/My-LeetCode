b4068dfe1336939f5005b49001098ce6abce55ab
Yu Shu <ys2530@nyu.edu>
2016-09-23 17:41:25 -0400

    [264. Ugly Number II][Time Limit Exceeded]committed by Anthony

--- 264-Ugly-Number-II/solution.java
+++ 264-Ugly-Number-II/solution.java
@@ -6,7 +6,7 @@
 
 // s1: brute force, check from 1, if it is ugly number, add to an arrayList, and stop when the arrayList.size get to n
 // O(n), O(n)
-
+// time limit exceeded
 
 public class Solution {
     public int nthUglyNumber(int n) {
@@ -22,10 +22,26 @@ public class Solution {
     }
     
     public boolean isUgly(int num) {
+        while (num != 1) {
+            if (num % 2 != 0 && num % 3 != 0 && num % 5 != 0) {
+                return false;
+            } else if (num % 2 == 0){
+                num /= 2;
+            } else if (num % 3 == 0) {
+                num /= 3;
+            } else if (num % 5 == 0) {
+                num /= 5;
+            }
+        }
+        return true;
+    }
+/*    
+    public boolean isUgly(int num) {
         // corner case
         if (num <= 0) {
             return false;
         }
+        
         while (num % 2 == 0) {
             num /= 2;
         }
@@ -37,4 +53,20 @@ public class Solution {
         }
         return num == 1;
     }
+    */
 }
+
+// s2:
+//
+/*
+public class Solution {
+    public int nthUglyNumber(int n) {
+        
+        
+        
+        
+        
+        
+    }
+}
+*/
