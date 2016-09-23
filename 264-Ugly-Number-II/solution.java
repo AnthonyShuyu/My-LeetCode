de164440202e6864fcc99c277312f167f394fef8
Yu Shu <ys2530@nyu.edu>
2016-09-23 17:42:03 -0400

    [264. Ugly Number II][Time Limit Exceeded]committed by Anthony

--- 264-Ugly-Number-II/solution.java
+++ 264-Ugly-Number-II/solution.java
@@ -22,21 +22,6 @@ public class Solution {
     }
     
     public boolean isUgly(int num) {
-        while (num != 1) {
-            if (num % 2 != 0 && num % 3 != 0 && num % 5 != 0) {
-                return false;
-            } else if (num % 2 == 0){
-                num /= 2;
-            } else if (num % 3 == 0) {
-                num /= 3;
-            } else if (num % 5 == 0) {
-                num /= 5;
-            }
-        }
-        return true;
-    }
-/*    
-    public boolean isUgly(int num) {
         // corner case
         if (num <= 0) {
             return false;
@@ -53,7 +38,6 @@ public class Solution {
         }
         return num == 1;
     }
-    */
 }
 
 // s2:
