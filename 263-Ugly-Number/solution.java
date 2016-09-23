564237f71bb3e33c7d08fb93e9afb383f15330aa
Yu Shu <ys2530@nyu.edu>
2016-09-23 17:06:19 -0400

    [263. Ugly Number][Accepted]committed by Anthony

---
+++ 263-Ugly-Number/solution.java
@@ -0,0 +1,31 @@
+/**
+ * 263. Ugly Number
+ * 
+ * 
+ */
+
+
+// s1: brute force, divide 2,3,5 for many times and stop when finally get 1
+// O(n), O(1)
+
+public class Solution {
+    public boolean isUgly(int num) {
+        // corner case
+        if (num <= 0) {
+            return false;
+        }
+                
+        while (num != 1) {
+            if (num % 2 != 0 && num % 3 != 0 && num % 5 != 0) {
+                return false;
+            } else if (num % 2 == 0){
+                num /= 2;
+            } else if (num % 3 == 0) {
+                num /= 3;
+            } else if (num % 5 == 0) {
+                num /= 5;
+            }
+        }
+        return true;
+    }
+}
