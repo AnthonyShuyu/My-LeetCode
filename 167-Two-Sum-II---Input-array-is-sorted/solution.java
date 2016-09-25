9094508ec1c2e4b3d77389e035f25f726b32674a
Yu Shu <ys2530@nyu.edu>
2016-09-25 12:20:19 -0400

    [167. Two Sum II - Input array is sorted][Accepted]committed by Anthony

--- 167-Two-Sum-II---Input-array-is-sorted/solution.java
+++ 167-Two-Sum-II---Input-array-is-sorted/solution.java
@@ -8,6 +8,7 @@
 // s1: two pointers
 // O(n), O(1)
 
+/*
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         // corner case
@@ -29,6 +30,33 @@ public class Solution {
                 result[0] = left + 1;
                 result[1] = right + 1;
                 break;
+            }
+        }
+        return result;
+    }
+}
+*/
+
+
+// s2: use a HashMap
+// O(n), O(n)
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        // corner case
+        if (numbers == null || numbers.length == 0) {
+            return null;
+        }
+        int[] result = new int[2];
+        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < numbers.length; i++) {
+            if (hashMap.containsKey(target - numbers[i])) {
+                result[0] = hashMap.get(target - numbers[i]) + 1;
+                result[1] = i + 1;
+                break;
+            } else {
+                hashMap.put(numbers[i], i);
             }
         }
         return result;
