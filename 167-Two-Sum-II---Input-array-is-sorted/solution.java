0aba79c68e9a782b40491f88089596440ff8e464
Yu Shu <ys2530@nyu.edu>
2016-09-25 12:44:51 -0400

    [167. Two Sum II - Input array is sorted][Compile Error]committed by Anthony

--- 167-Two-Sum-II---Input-array-is-sorted/solution.java
+++ 167-Two-Sum-II---Input-array-is-sorted/solution.java
@@ -41,6 +41,7 @@ public class Solution {
 // s2: use a HashMap
 // O(n), O(n)
 
+/*
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         // corner case
@@ -60,5 +61,66 @@ public class Solution {
             }
         }
         return result;
+    }
+}
+*/
+
+
+
+// s3: binary search
+// O(nlogn), O(1)
+ 
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        // corner case
+        if (numbers == null || numbers.length == 0) {
+            return null;
+        }
+        int[] result = new int[2];        
+        for (int i = 0; i < numbers.length; i++) {
+            int num = target - numbers[i];
+            int index = binarySearch(numbers, num);
+            if (index == -1 || index == i) {
+                return result;
+            } else if (index < i){
+                swap(index, i);
+            }
+            result[0] = i + 1;
+            result[1] = index + 1;
+        }
+        return result;
+    }
+    
+    public void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    
+    public int binarySearch(int[] numbers, int n) {
+        // corner case
+        if (numbers == null || numbers.length == 0) {
+            return false;
+        }
+        int start = 0;
+        int end = numbers.length - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (numbers[mid] == n) {
+                return mid;
+            } else if (numbers[mid] < n) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (numbers[start] == n) {
+            return start;
+        }
+        if (numbers[end] == n) {
+            return end;
+        }
+        return -1;
     }
 }
