58435334ee515fbb761fb3316e7c4beecb5970fb
Yu Shu <ys2530@nyu.edu>
2016-09-28 15:47:51 -0400

    [168. Excel Sheet Column Title][Accepted]committed by Anthony

---
+++ 168-Excel-Sheet-Column-Title/solution.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ * 168. Excel Sheet Column Title
+ * 
+ * 
+ */
+
+
+// s1: brute force, math
+// O(n), O(1)
+
+public class Solution {
+    public String convertToTitle(int n) {
+        // corner case
+        if (n <= 0) {
+            return null;
+        }
+        int num = n;
+        int total = 'z' - 'a' + 1;
+        StringBuilder sb = new StringBuilder();
+        while ((num - 1) / total != 0) {
+            sb.append((char)((num - 1) % total + 'A'));
+            num = (num - 1) / total;
+        }
+        sb.append((char)((num - 1) % total + 'A'));
+        return sb.reverse().toString();         
+    }
+}
