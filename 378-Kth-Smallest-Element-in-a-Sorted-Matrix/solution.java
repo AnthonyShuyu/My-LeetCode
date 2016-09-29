4ba56d671787e66b4d5e67b18e748dbe4e5c0890
Yu Shu <ys2530@nyu.edu>
2016-09-29 01:53:59 -0400

    [378. Kth Smallest Element in a Sorted Matrix][Compile Error]committed by Anthony

--- 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
+++ 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
@@ -81,6 +81,7 @@ public class Solution {
 // O(klogk), O(n^2)
 // tricky
 
+/*
 public class Solution {
     
     // implement a new class called Number
@@ -147,3 +148,70 @@ public class Solution {
         return false;
     }
 }        
+*/
+
+// s3 again, use minHeap and hash matrix
+// O(klogk), O(n^2)
+// tricky
+
+public class Solution {
+    
+    class Number{
+        int x;
+        int y;
+        int val;
+        public Number(int x, int y, int val) {
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
+    }
+    
+    public int kthSmallest(int[][] matrix, int k) {
+        // corner case
+        if (matrix == null || matrix.length == 0) {
+            return -1;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return -1;
+        }
+        Queue<Number> queue = new PriorityQueue<Number>(new Comparator<Number>() {
+                @Override
+                public int compare(Number n1, Number n2) {
+                    return n1.val - n2.val;
+                }
+            });
+        
+        boolean[][] hash = new boolean[matrix.length][matrix[0].length];
+        queue.offer(new Number(0, 0, matrix[0][0]));
+        hash[0][0] = true;
+        
+        for (int i = 0; i < k - 1; i++) {
+            Number num = queue.poll();
+            int x = num.x;
+            int y = num.y;
+            
+            int nx1 = x;
+            int ny1 = y + 1;
+            if (isValid(nx1, ny1, matrix, hash)) {
+                queue.offer(nx1, ny1, matrix[nx1][ny1]);                
+                hash[nx1][ny1] = true;
+            }
+            
+            int nx2 = x + 1;
+            int ny2 = y;
+            if (isValid(nx2, ny2, matrix, hash)) {
+                queue.offer(nx2, ny2, matrix[nx2][ny2]);
+                hash[nx2][ny2] = true;
+            }
+        }
+        return queue.peek().val;
+        
+    }
+    
+    public boolean isValid(int nx1, int ny1, int[][] matrix, boolean[][] hash) {
+        if (nx1 < matrix.length && ny1 < matrix[0].length && !hash[nx1][ny1]) {
+            return true;
+        }
+    }
+}
