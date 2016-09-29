313eb40f97a99cea320635820b978438a4a22b17
Yu Shu <ys2530@nyu.edu>
2016-09-29 18:11:35 -0400

    [313. Super Ugly Number][Time Limit Exceeded]committed by Anthony

--- 313-Super-Ugly-Number/solution.java
+++ 313-Super-Ugly-Number/solution.java
@@ -10,6 +10,7 @@
 // O(n), O(1)
 // time limit exceeded
 
+/*
 public class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
         // corner case
@@ -40,5 +41,44 @@ public class Solution {
             }
         }
         return num == 1;
+    }
+}
+*/
+
+
+// s2: use heap + hashSet
+// O(n), O(n)
+
+public class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        // corner case
+        if (primes == null || primes.length == 0) {
+            return -1;
+        }
+        Queue<Integer> queue = new PriorityQueue<Integer>();
+        Set<Integer> hashSet = new HashSet<Integer>();
+        
+        queue.offer(1);
+        hashSet.add(1);
+        
+        int count = 0;
+        int result = 0;
+        
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            result = num;
+            count++;
+            if (count == n) {
+                break;
+            }
+            for (int i = 0; i < primes.length; i++) {
+                int new_num = num * primes[i];
+                if (!hashSet.contains(new_num)) {
+                    queue.offer(new_num);
+                    hashSet.add(new_num);
+                }
+            }
+        }
+        return result;
     }
 }
