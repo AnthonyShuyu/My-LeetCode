a66e36c61b963cb56fccf8836004f71e650a8893
Yu Shu <ys2530@nyu.edu>
2016-09-29 16:42:56 -0400

    [78. Subsets][Compile Error]committed by Anthony

---
+++ 78-Subsets/solution.java
@@ -0,0 +1,34 @@
+/**
+ * 78. Subsets
+ * 
+ * 
+ */
+
+// s1: DFS
+// O(2^n * n), O(n)
+
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        //  corner case
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length == 0) {
+            return list;
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        helper(result, list, nums, 0);
+        return result;
+    }
+    
+    public void helper(List<List<Integer>> result, List<Integer> list, int[] nums, int pos) {
+        List<Integer> temp = new ArrayList<Integer>(list);
+        result.add(temp);
+        
+        for (int i = pos; i < nums.length; i++) {
+            list.add(nums[i]);
+            helper(result, list, nums, i + 1);
+            list.remove(list.size() - 1);
+        }
+        
+    }
+}
