f0457d8eac1cd8cec86b4f3dafe1ea867b71d6e5
Yu Shu <ys2530@nyu.edu>
2016-09-29 16:04:11 -0400

    [31. Next Permutation][Compile Error]committed by Anthony

--- 31-Next-Permutation/solution.java
+++ 31-Next-Permutation/solution.java
@@ -52,6 +52,7 @@ public class Solution {
 // *s2: brute force, 2 times loop
 // O(n), O(n)
 
+/*
 public class Solution {
     public void nextPermutation(int[] nums){
         if (nums == null || nums.length == 0) {
@@ -98,3 +99,58 @@ public class Solution {
         }
     }
 }
+*/
+
+
+// s2 again, OA practice
+// O(n), O(n)
+
+
+public class Solution {
+    public void nextPermutation(int[] nums){
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        // find the first increasing index
+        int index1 = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+        if (index1 = -1) {
+            reverse(0, nums.length - 1, nums);
+        }
+        
+        // find the first index that > index1
+        int index2 = -1;
+        for (int i = nums.length - 1; i > index1; i--) {
+            if (nums[i] > nums[index1]) {
+                index2 = i;
+                break;
+            }
+        }
+        
+        swap(index1, index2, nums);
+        reverse(index1 + 1, index2, nums);
+    }
+    
+    public void reverse(int start, int end, int[] nums) {
+        if (start >= end) {
+            return;
+        }
+        for (int i = start, j = end; i < j; i++, j--) {
+            int num = nums[i];
+            num[i] = nums[j];
+            nums[j] = num;
+        }
+    }
+    
+    public void swap(int index1, int index2, int[] nums) {
+        int num = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = num;
+    }
+    
+}    
