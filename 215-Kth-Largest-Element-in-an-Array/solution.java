a0209a9da2faa14ab3b8fa173ae543f313159aa3
Yu Shu <ys2530@nyu.edu>
2016-09-30 03:32:40 -0400

    [215. Kth Largest Element in an Array][Runtime Error]committed by Anthony

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -190,8 +190,10 @@ public class Solution {
         }       
         if (left - start < k) {
             return helper(left, end, nums, k - (left - start));
-        } else {
+        } else if (left - start > k){
             return helper(start, left - 1, nums, k);
+        } else {
+            return nums[left + 1];
         }
     }
     
