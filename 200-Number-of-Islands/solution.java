0c3f85a13b4720c5bf65fe84f817efa7b2257e75
Yu Shu <ys2530@nyu.edu>
2016-10-03 19:54:35 -0400

    [200. Number of Islands][Compile Error]committed by Anthony

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -8,7 +8,9 @@
 
 // s1: union find, HashMap
 // O(n * m), O(1)
+// failed
 
+/*
 public class Solution {
     
     Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
@@ -48,6 +50,21 @@ public class Solution {
                 }
             }
         }
+        
+            // 3rd traverse, union find
+    for (int i = 0; i < grid.length; i++) {
+        for (int j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] == '1') {
+                union(i * n + j, hashMap.get(i * n + j));
+            }
+        }
+    }
+    
+     Set<Integer> hashSet = new HashSet<Integer>();
+     for (int value: hashMap.values()) {
+        hashSet.add(value);
+        }
+        return hashSet.size();
     }
     
     // O(1) find
@@ -72,20 +89,112 @@ public class Solution {
             hashMap.put(parentX, parentY);
         }
     }
+}
+*/
+
+// s2: use arrayList to traverse every node
+// O(n * m) , O(n * m)
+// failed
+
+/*
+public class Solution {
+    public int numIslands(char[][] grid) {
+        // corner case
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        if (grid[0] == null || grid[0].length == 0) {
+            return 0;
+        }
+        
     
-    // 3rd traverse, union find
-    for (int i = 0; i < grid.length; i++) {
-        for (int j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] == '1') {
-                union(i * n + j, hashMap.get(i * n + j));
+    }
+    
+    public List<Integer> getList(int x, int y, char[][] grid) {
+        int[] dx = {0, 1};
+        int[] dy = {1, 0};
+    }
+}    
+*/
+
+// s3: use bfs
+// O(n * m), O(n * m)
+
+public class Solution {
+    public int numIslands(char[][] grid) {
+        // corner case
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        if (grid[0] == null || grid[0].length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        Queue<Integer> queue;
+        Set<Integer> hashSet = new HashSet<Integer>();
+        int count = 0;
+        /*
+        for (int i = 0; i < m * n; i++) {
+            if (grid[i / n][i % n] == '1') {
+                if (!hashSet.contains(i)) {
+                    hashSet.add(i);
+                    result++;
+                    queue = new LinkedList<Integer>();
+                    queue.offer(i);
+                    while (!queue.isEmpty()) {
+                        int num = queue.poll();
+                        // check right and down
+                        if (i / n - 1 >= 0 && grid[i / n - 1][i % n] == '1' && hashSet.contains()) {
+                            queue.offer();
+                        }
+                        
+                        if (i % n < grid[0].length && grid[i / n][i % n] == '1') {
+                            queue.offer();
+                        }
+                        
+                    }
+                }
             }
         }
-    }
+        */
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    if (!hashSet.contains(change(i, j, grid))) {
+                        hashSet.add(change(i, j, grid));
+                        queue = new LinkedList<Integer>();
+                        count++;
+                        queue.offer(change(i, j , grid));
+                        while (!queue.isEmpty()) {
+                            int num = queue.poll();
+                            // row
+                            int row = num / n;
+                            int col = num % n;
+                            for (int k = 0; k < dx.length; k++) {
+                                drow = row + dx[k];
+                                dcol = col + dy[k];
+                                if (drow < 0 || drow >= m || dcol < 0 || dcol >= n) {
+                                    continue;
+                                }
+                                if (grid[drow][dcol] == '1' && !hashSet.contains(change(drow, dcol, grid))) {
+                                    hashSet.add(change(drow, dcol, grid));
+                                    queue.offer(change(drow, dcol, grid));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+     }
     
-    Set<Integer> hashSet = new HashSet<Integer>();
-    for (int value: hashMap.values()) {
-        hashSet.add(value);
-    }
-    return hashSet.size();
     
+    public int change(int x, int y, char[][] grid) {
+        return x * grid[0].length + y;
+    }
 }
