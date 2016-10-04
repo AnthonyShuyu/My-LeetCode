e06a9a5a42495bfd199ab02c4050d43821dafc83
Yu Shu <ys2530@nyu.edu>
2016-10-04 14:20:55 -0400

    [305. Number of Islands II][Accepted]committed by Anthony

--- 305-Number-of-Islands-II/solution.java
+++ 305-Number-of-Islands-II/solution.java
@@ -68,6 +68,7 @@ public class Solution {
 
 // s2: union find
 // O(k * m * n), O(m * n)
+// time limit exceeded
 
 public class Solution {
     Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
@@ -80,37 +81,49 @@ public class Solution {
         
         int[] nums = new int[positions.length];
         
-        
+        int count = 0;
         for (int i = 0; i < nums.length; i++) {
+            count++;
             nums[i] = positions[i][0] * n + positions[i][1];
             hashMap.put(nums[i], nums[i]);
             if (nums[i] / n > 0 && hashMap.containsKey(nums[i] - n)) {
+                if (find(nums[i]) != find(nums[i] - n)) {
+                    count--;
+                }
                 union(nums[i] - n, nums[i]);
             }
             if (nums[i] % n > 0 && hashMap.containsKey(nums[i] - 1)) {
+                if (find(nums[i]) != find(nums[i] - 1)) {
+                    count--;
+                }
                 union(nums[i] - 1, nums[i]);
             }
             if (nums[i] % n < n - 1 && hashMap.containsKey(nums[i] + 1)) {
+                if (find(nums[i]) != find(nums[i] + 1)) {
+                    count--;
+                }
                 union(nums[i], nums[i] + 1);
             } 
             if (nums[i] / n < m - 1 && hashMap.containsKey(nums[i] + n)) {
+                if (find(nums[i]) != find(nums[i] + n)) {
+                    count--;
+                }
                 union(nums[i], nums[i] + n);
             }
-            int count = countIslands(hashMap);
+            // int count = countIslands(hashMap);
             result.add(count);
         }
         return result;
         
     }    
     
-    
-    public int countIslands(Map<Integer, Integer> hashMap) {
-        Set<Integer> hashSet = new HashSet<Integer>();
-        for (int key : hashMap.keySet()) {
-            hashSet.add(find(key));
-        }
-        return hashSet.size();
-    }
+    // public int countIslands(Map<Integer, Integer> hashMap) {
+    //     Set<Integer> hashSet = new HashSet<Integer>();
+    //     for (int key : hashMap.keySet()) {
+    //         hashSet.add(find(key));
+    //     }
+    //     return hashSet.size();
+    // }
     
     
     
@@ -137,3 +150,46 @@ public class Solution {
         }
     }
 }
+
+// s3*: union find again
+// O(m * n + k), O(m * n)
+// tircky
+
+/*
+public class Solution {
+    Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+    public List<Integer> numIslands2(int m, int n, int[][] positions) {
+        // corner case
+        List<Integer> result = new ArrayList<Integer>();
+        if (positions == null || positions.length == 0 || positions[0] == null || positions[0].length == 0) {
+            return result;
+        }
+        
+        int[][] map = new int[m][n];
+        
+        
+        
+    }
+    
+    
+    public int find(int x) {
+        int parent = x;
+        while (parent != hashMap.get(parent)) {
+            parent = hashMap.get(parent);
+        }
+        int next;
+        while (x != hashMap.get(x)) {
+            next = hashMap.get(x);
+            hashMap.put(x, parent);
+            x = next;
+        }
+        return parent;
+    }
+    
+    public void union(int x, int y) {
+        int parentX = hashMap.get(x);
+        int parentY = hashMap.get(y);
+        
+    }
+}
+*/
