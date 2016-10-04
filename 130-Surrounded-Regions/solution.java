f67145c71171d84d3ffd888cfcec17f1bc0bf230
Yu Shu <ys2530@nyu.edu>
2016-10-04 18:12:48 -0400

    [130. Surrounded Regions][Compile Error]committed by Anthony

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -85,8 +85,9 @@ public class Solution {
 // s1: dfs again
 // O(m * n), O(m * n)
 // tricky, from the edges 'O', change all the adjacent 'O' to 'D', then change back at last
-// 
+// stack overflow error
 
+/*
 public class Solution {
     public void solve(char[][] board) {
         // corner case
@@ -126,9 +127,6 @@ public class Solution {
                 }
             }
         }
-        
-        
-        
     }
     
     public void dfs(int x, int y, char[][] board) {
@@ -143,3 +141,89 @@ public class Solution {
         }
     }
 }    
+*/
+
+// s2: bfs, since use dfs will get Stack Overflow error
+// O(m * n), O(m * n)
+
+public class Solution {
+    public void solve(char[][] board) {
+        // corner case
+        if (board == null || board.length == 0) {
+            return;
+        }
+        if (board[0] == null || board[0].length == 0) {
+            return;
+        }
+    }
+    
+        int m = board.length;
+        int n = board[0].length;
+        
+        Queue<Integer> queue = new LinkedList<Integer>();
+        
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                queue.offer(change(i, 0, board));
+            }
+            if (board[i][n - 1] == 'O') {
+                queue.offer(change(i, n - 1, board));
+            }
+        }
+        
+        for (int i = 1; i < n - 1; i++) {
+            if (board[0][i] == 'O') {
+                queue.offer(change(0, i , board));
+            }
+            if (board[m - 1][i] == 'O') {
+                queue.offer(change(m - 1, i, board));
+            }
+        }
+        
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+        
+        
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            int x = num / n;
+            int y = num % n;
+            if (board[x][y] == 'O') {
+                board[x][y] = 'A';
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    if (valid(nx, ny, board)) {
+                        queue.offer(change(nx, ny, board));
+                    }                
+                }
+            }
+        }
+        
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        
+    }
+    
+    public int change(int x, int y, char[][] board) {
+        int n = board[0].length;
+        return x * n + y;
+    }
+    
+    public boolean valid(int x, int y, char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        if (x >= 0 && x < m && y >= 0 && y < n && board[x][y] == 'O') {
+            return true;
+        }
+        return false;
+    }
+}
