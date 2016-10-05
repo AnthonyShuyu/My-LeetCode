e3665ae698b9ebf40dc3a901d8efbb3d45db1997
Yu Shu <ys2530@nyu.edu>
2016-10-05 16:43:02 -0400

    [211. Add and Search Word - Data structure design][Compile Error]committed by Anthony

--- 211-Add-and-Search-Word---Data-structure-design/solution.java
+++ 211-Add-and-Search-Word---Data-structure-design/solution.java
@@ -81,7 +81,7 @@ public class WordDictionary {
     // Returns if the word is in the data structure. A word could
     // contain the dot character '.' to represent any one letter.
     public boolean search(String word) {
-        TrieNode node = t;
+        TrieNode node = t.root;
         // corner case
         if (word == null || word.length() == 0) {
             return false;
