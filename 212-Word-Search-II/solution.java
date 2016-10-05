60162e8e262998049d48bc16d4ff0573e64f2cdc
Yu Shu <ys2530@nyu.edu>
2016-10-05 14:41:30 -0400

    [212. Word Search II][Compile Error]committed by Anthony

--- 212-Word-Search-II/solution.java
+++ 212-Word-Search-II/solution.java
@@ -219,7 +219,7 @@ public class Solution {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 String s = "";
-                String string = find(t, board, i, j, s));
+                String string = find(t, board, i, j, s);
                 if (string != null) {
                     result.add(string);
                 }    
