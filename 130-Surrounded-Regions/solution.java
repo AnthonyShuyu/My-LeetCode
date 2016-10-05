e053b572ec6afbf1264d62a230a4bbacc61297b5
Yu Shu <ys2530@nyu.edu>
2016-10-04 21:54:57 -0400

    [130. Surrounded Regions][Runtime Error]committed by Anthonyd()

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -7,81 +7,6 @@
  * 
  */
 
-// s1: dfs
-// O(n * m), O(n * m)
-// Stack overflow error
-
-/*
-public class Solution {
-    Set<Integer> hashSet = new HashSet<Integer>();
-    public void solve(char[][] board) {
-        // corner case
-        if (board == null || board.length == 0) {
-            return;
-        }
-        if (board[0] == null || board[0].length == 0) {
-            return;
-        }
-        int m = board.length;
-        int n = board[0].length;
-        
-        char[][] map = new char[m][n];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                map[i][j] = board[i][j];
-            }
-        }
-        
-        
-        // do dfs check
-        
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 'O' && !hashSet.contains(i * n + j)) {
-                    if (dfsCheck(i, j, map)) {
-                        dfs(i, j, board);
-                    }                
-                }
-            }
-        }
-    }
-    
-    public boolean dfsCheck(int i, int j, char[][] map) {
-        int m = map.length;
-        int n = map[0].length;
-        if (map[i][j] == 'O') {
-            hashSet.add(i * n + j);
-        }
-        
-        if (map[i][j] == 'O' && (i == 0 || i == m - 1 || j == 0 || j == n - 1)) {
-            return false;
-        }
-        if (map[i][j] == 'O') {
-            map[i][j] = 'X';
-            return dfsCheck(i - 1, j, map) && dfsCheck(i, j + 1, map) && dfsCheck(i + 1, j, map) && dfsCheck(i, j - 1, map);
-        } else {
-            return true;
-        }
-    }
-    
-    public void dfs(int i, int j, char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
-        if (i < 0 || i >= m || j < 0 || j >= n) {
-            return;
-        }
-        
-        if (board[i][j] == 'O') {
-            board[i][j] = 'X';
-        dfs(i - 1, j, board);
-        dfs(i + 1, j, board);
-        dfs(i, j - 1, board);
-        dfs(i, j + 1, board);
-        }
-    }
-}
-*/
-
 // s1: dfs again
 // O(m * n), O(m * n)
 // tricky, from the edges 'O', change all the adjacent 'O' to 'D', then change back at last
@@ -146,6 +71,7 @@ public class Solution {
 // s2: bfs, since use dfs will get Stack Overflow error
 // O(m * n), O(m * n)
 
+/*
 public class Solution {
     public void solve(char[][] board) {
         // corner case
@@ -225,5 +151,124 @@ public class Solution {
             return true;
         }
         return false;
+    }
+}
+*/
+
+// s3: use union find
+// O(m * n), O(m * n)
+
+
+public class Solution {
+    
+    Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+    public void solve(char[][] board) {
+        // corner case
+        if (board == null || board.length == 0) {
+            return;
+        }
+        if (board[0] == null || board[0].length == 0) {
+            return;
+        }
+        int m = board.length;
+        int n = board[0].length;
+        
+        // first traverse, put element and itself
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    hashMap.put(change(i, j, board), change(i, j, board));
+                }
+            }
+        }
+        
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        
+        // second traverse, union adjacent elements
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    for (int k = 0; k < 4; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        if (isValid(nx, ny, board)) {
+                            int index1 = i * n + j;
+                            int index2 = nx * n + ny;
+                            if (find(index1) != find(index2)) {
+                                union(index1, index2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        // use hashSet to store the elements in the 4 sides
+        Set<Integer> hashSet = new HashSet<Integer>();
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                hashSet.add(find(i * n));
+            }
+            if (board[i][n - 1] == 'O') {
+                hashSet.add(find(i * n + n - 1));
+            }
+        }
+        
+        for (int i = 1; i < n - 1; i++) {
+            if (board[0][i] == 'O') {
+                hashSet.add(find(i));
+            }
+            if (board[m - 1][i] == 'O') {
+                hashSet.add(find((m - 1) * n + i));
+            }
+        }        
+        
+        // third traverse, change 'O' to 'X'
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!hashSet.contains(find(i * n + j))) {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        
+    }
+    
+    public boolean isValid(int x, int y, char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        if (x >= 0 && x < m && y >= 0 && y < n && board[x][y] == 'O') {
+            return true;
+        }
+        return false;
+    }
+    
+    public int change(int x, int y, char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        return x * n + y;
+    }
+    
+    public int find(int x) {
+        int parent = x;
+        while (parent != hashMap.get(parent)) {
+            parent = hashMap.get(parent);
+        }
+        int next;
+        while (x != hashMap.get(x)) {
+            next = hashMap.get(x);
+            hashMap.put(x, parent);
+            x = next;
+        }
+        return parent;
+    }
+    
+    public void union(int x, int y) {
+        int parentX = hashMap.get(x);
+        int parentY = hashMap.get(y);
+        if (parentX != parentY) {
+            hashMap.put(parentX, parentY);
+        }
     }
 }
