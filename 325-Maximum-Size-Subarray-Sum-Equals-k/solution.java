6b7abdda928f247275ef80384db0e0b21b75e8b7
Yu Shu <ys2530@nyu.edu>
2016-10-08 15:03:31 -0400

    [325. Maximum Size Subarray Sum Equals k][Accepted]committed by Anthony

--- 325-Maximum-Size-Subarray-Sum-Equals-k/solution.java
+++ 325-Maximum-Size-Subarray-Sum-Equals-k/solution.java
@@ -8,6 +8,7 @@
 // s1: brute force
 // O(n^2), O(1)
 
+/*
 public class Solution {
     public int maxSubArrayLen(int[] nums, int k) {
         // corner case
@@ -22,6 +23,42 @@ public class Solution {
                 sum += nums[j];
                 if (sum == k) {
                     max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+        return max;
+    }
+}
+*/
+
+
+// s2: prefix sum: subarray sum equals to the difference betweeen sum of two index from 0
+// O(n^2), O(n)
+// tircky
+
+public class Solution {
+    public int maxSubArrayLen(int[] nums, int k) {
+        // corner case
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int[] sums = new int[nums.length];
+        
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            sums[i] = sum;
+        }
+        
+        int max = 0;
+        for (int i = 0; i < sums.length; i++) {
+            if (sums[i] == k) {
+                max = Math.max(i + 1, max);
+            }
+            for (int j = i + 1; j < sums.length; j++) {
+                if (sums[j] - sums[i] == k) {
+                    max = Math.max(j - i, max);
                 }
             }
         }
