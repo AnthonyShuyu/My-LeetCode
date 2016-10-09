69a7f132b0dac0d84337946fb8dd7644cb7aee22
Yu Shu <ys2530@nyu.edu>
2016-10-09 01:55:47 -0400

    [295. Find Median from Data Stream][Compile Error]committed by Anthony

--- 295-Find-Median-from-Data-Stream/solution.java
+++ 295-Find-Median-from-Data-Stream/solution.java
@@ -9,6 +9,7 @@
 // O(n^2logn), O(n)
 // time limit exceeded
 
+/*
 public class MedianFinder {
 
 
@@ -32,6 +33,66 @@ public class MedianFinder {
         return (double)(list.get(m / 2) + list.get((m - 1) / 2)) / 2;
     }
 };
+*/
+
+
+// s2: use 2 heaps: a max heap and a min heap
+// O(nlogn), O(n)
+
+public class MedianFinder {
+    
+    Queue<Integer> minQueue = new PriorityQueue<Integer>();
+    Queue<Integer> maxQueue = new PriotityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer i1, Integer i2) {
+                return i2 - i1;
+            }
+        });
+    
+    
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+        if (minQueue.isEmpty() && maxQueue.isEmpty()) {
+            maxQueue.offer(num);
+        } else if (minQueue.size() == maxQueue.size()){
+            if (num <= maxQueue.peek()) {
+                maxQueue.offer(num);
+            } else if (num >= minQueue.peek()) {
+                minQueue.offer(num);
+            } else {
+                maxQueue.offer(num);
+            }
+        } else if (minQueue.size() > maxQueue.size()) {
+            if (num <= minQueue.peek()) {
+                maxQueue.offer(num);
+            } else {
+                minQueue.offer(num);
+                maxQueue.offer(minQueue.poll());
+            }
+        } else if (maxQueue.size() > minQueue.size()) {
+            if (num >= maxQueue.peek()) {
+                minQueue.offer(num);
+            } else {
+                maxQueue.offer(num);
+                minQueue.offer(maxQueue.poll());
+            }
+        }
+    }
+    
+    // Returns the median of current data stream
+    public double findMedian() {
+        if (maxQueue.size() > minQueue.size()) {
+            return maxQueue.peek();
+        } else if (maxQueue.size() < minQueue.size()) {
+            return minQueue.peek();
+        } else {
+            return (double)((maxQueue.peek() + minQueue.peek()) / 2);
+        }
+        
+    }
+    
+}
+
 
 // Your MedianFinder object will be instantiated and called as such:
 // MedianFinder mf = new MedianFinder();
