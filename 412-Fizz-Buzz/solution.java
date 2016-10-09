b3084aa1bee7f9b6974ba6c9a8240934ff82c8a8
Yu Shu <ys2530@nyu.edu>
2016-10-09 02:03:43 -0400

    [412. Fizz Buzz][Accepted]committed by Anthony

--- 412-Fizz-Buzz/solution.java
+++ 412-Fizz-Buzz/solution.java
@@ -20,7 +20,7 @@ public class Solution {
             } else if (i % 5 == 0) {
                 result.add("Buzz");
             } else {
-                result.add(i);
+                result.add(String.valueOf(i));
             }
         }
         return result;
