6398767a9971d4b13f6a6b95b5e6b3bd136447c3
Yu Shu <ys2530@nyu.edu>
2016-10-10 16:13:00 -0400

    [145. Binary Tree Postorder Traversal][Accepted]committed by Anthony

--- 145-Binary-Tree-Postorder-Traversal/solution.java
+++ 145-Binary-Tree-Postorder-Traversal/solution.java
@@ -20,6 +20,8 @@
  
 // s1: divide and conquer
 // O(n), O(n) 
+
+/*
 public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<Integer>();
@@ -37,4 +39,31 @@ public class Solution {
         
         return result;
     }
+}
+*/
+
+// s2: use recursion and traverse
+// O(n), O(n)
+
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        // corner case
+        if (root == null) {
+            return result;
+        }
+        helper(result, root);        
+        return result;        
+    }
+    
+    public void helper(List<Integer> result, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        
+        helper(result, root.left);
+        helper(result, root.right);
+        result.add(root.val);
+    }
+    
 }
