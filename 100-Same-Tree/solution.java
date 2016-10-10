eff39c99354037e91ad49717c10e08d0ce18f68c
Yu Shu <ys2530@nyu.edu>
2016-10-10 19:24:20 -0400

    [100. Same Tree][Accepted]committed by Anthony

--- 100-Same-Tree/solution.java
+++ 100-Same-Tree/solution.java
@@ -21,6 +21,7 @@
 // s1: dfs, recursion, divide and conquer
 // O(n), O(1)
 
+/*
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         // corner case
@@ -37,4 +38,53 @@ public class Solution {
         
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
+}
+*/
+
+
+// s2: serialize a tree to a String, to compare the two Strings
+// O(n), O(n)
+
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // corner case      
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        
+        String s1 = serializeTree(p);
+        String s2 = serializeTree(q);
+        
+        return s1.equals(s2);
+    }
+    
+    public String serializeTree(TreeNode root) {
+        
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        
+        sb.append(root.val);
+        queue.offer(root.left);
+        queue.offer(root.right);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node == null) {
+                    sb.append(",#");
+                } else {
+                    sb.append("," + node.val);
+                    queue.offer(node.left);
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return sb.toString();
+    }
+    
 }
