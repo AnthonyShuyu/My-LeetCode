5686752331fccd1e46b053c4f22886b1cadd5c16
Yu Shu <ys2530@nyu.edu>
2016-10-10 16:33:35 -0400

    [226. Invert Binary Tree][Accepted]committed by Anthony

---
+++ 226-Invert-Binary-Tree/solution.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ * 226. Invert Binary Tree
+ * 
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+// s1: divide and conquer
+// O(n), O(n)
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        // Divide
+        TreeNode leftNode = invertTree(root.left);
+        TreeNode rightNode = invertTree(root.right);
+        
+        // Conquer
+        // TreeNode tempNode = leftNode;
+        // leftNode = rightNode;
+        // rightNode = tempNode;
+        
+        root.left = rightNode;
+        root.right = leftNode;
+        return root;
+    }
+}
