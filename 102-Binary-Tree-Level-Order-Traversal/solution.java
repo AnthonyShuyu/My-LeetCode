b8e6ed0b328b374848838ab72bfb3bcc3ab0fa39
Yu Shu <ys2530@nyu.edu>
2016-10-09 21:31:13 -0400

    [102. Binary Tree Level Order Traversal][Compile Error]committed by Anthony

---
+++ 102-Binary-Tree-Level-Order-Traversal/solution.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ * 
+ * 102. Binary Tree Level Order Traversal
+ * 
+ * 
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+ 
+// s1: use Queue
+// O(n), O(n)
+
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // corner case
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root.val);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<Integer>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
