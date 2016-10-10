d91278d29b8d73cebd154ce02295c70a04e564b9
Yu Shu <ys2530@nyu.edu>
2016-10-09 21:58:31 -0400

    [102. Binary Tree Level Order Traversal][Compile Error]committed by Anthony

--- 102-Binary-Tree-Level-Order-Traversal/solution.java
+++ 102-Binary-Tree-Level-Order-Traversal/solution.java
@@ -56,11 +56,50 @@ public class Solution {
 
 */
 
-
-// s2: use DFS, but not with Stack
+// s2: use dummy node and Queue
 // O(n), O(n)
 
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // corner case
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        queue.offer(null);
+        
+        List<Integer> list = new ArrayList<Integer>();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(left);
+                }
+                if (node.right != null) {
+                    queue.offer(right);
+                }
+            } else {
+                if (list.size() == 0) {
+                    break;
+                }
+                queue.offer(null);
+                result.add(list);
+                list = new ArrayList<Integer>();
+            }
+        }
+        return result;            
+    }
+}
 
+
+// s3: use DFS, but not with Stack
+// O(n), O(n)
+
+/*
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         // corner case
@@ -97,3 +136,7 @@ public class Solution {
     }
     
 }
+*/
+
+
+/
