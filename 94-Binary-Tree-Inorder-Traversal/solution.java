b86115286269b9b235a4877b54b055ceb29fda47
Yu Shu <ys2530@nyu.edu>
2016-10-10 16:08:25 -0400

    [94. Binary Tree Inorder Traversal][Accepted]committed by Anthony

--- 94-Binary-Tree-Inorder-Traversal/solution.java
+++ 94-Binary-Tree-Inorder-Traversal/solution.java
@@ -19,6 +19,7 @@
 // s1: divide and conquer
 // O(n), O(n)
 
+/*
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<Integer>();
@@ -35,5 +36,32 @@ public class Solution {
         result.add(root.val);
         result.addAll(rightList);
         return result;
+    }
+}
+*/
+
+
+// s2: use recursion to traverse
+// O(n), O(n)
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        // corner case
+        if (root == null) {
+            return result;
+        }
+        helper(result, root);
+        return result;
+    }
+    
+    public void helper(List<Integer> result, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        
+        helper(result, root.left);
+        result.add(root.val);
+        helper(result, root.right);
     }
 }
