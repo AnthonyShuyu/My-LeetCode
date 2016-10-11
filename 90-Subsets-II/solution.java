2f50f3a2e784bbf0f1fa35021e4988ee05619262
Yu Shu <ys2530@nyu.edu>
2016-10-11 16:23:36 -0400

    [90. Subsets II][Accepted]committed by Anthony

---
+++ 90-Subsets-II/solution.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ * 90. Subsets II
+ * 
+ * 
+ */
+
+
+
+// s1: DFS
+// O(2^n * n), O(n)
+
+public class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        // corner case
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        Arrays.sort(nums);
+        List<Integer> list = new ArrayList<Integer>();
+        int pos = 0;
+        dfs(result, list, nums, pos);
+        return result;
+    }
+    
+    public void dfs(List<List<Integer>> result, List<Integer> list, int[] nums, int pos) {
+        result.add(new ArrayList<Integer>(list));
+        for (int i = pos; i < nums.length; i++) {
+            if (i != pos && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            list.add(nums[i]);
+            dfs(result, list, nums, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
