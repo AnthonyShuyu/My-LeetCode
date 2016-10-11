8d1f65f5b7a9b73161c71d97c940711f17da1768
Yu Shu <ys2530@nyu.edu>
2016-10-11 00:15:19 -0400

    [50. Pow(x, n)][Compile Error]committed by Anthony

--- 50-Pow(x,-n)/solution.java
+++ 50-Pow(x,-n)/solution.java
@@ -44,23 +44,23 @@
 public class Solution {
     public double myPow(double x, int n) {
         
-        long n = n;
+        long n1 = n;
         
-        if (n == 0) {
+        if (n1 == 0) {
             return 1;
         }
-        if (n == 1) {
+        if (n1 == 1) {
             return x;
         }
         
         boolean isNegative = false;
-        if (n < 0) {
+        if (n1 < 0) {
             isNegative = true;
-            n = n * -1;
+            n1 = n1 * -1;
         }
         
-        int k = n / 2;
-        int l = n - k * 2;
+        int k = n1 / 2;
+        int l = n1 - k * 2;
         
         double t1 = myPow(x, k);
         double t2 = myPow(x, l);
