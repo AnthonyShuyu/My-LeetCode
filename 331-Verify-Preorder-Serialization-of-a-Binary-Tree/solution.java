fe0f48fb5870264c7cbb87f727943a6f232218a4
Yu Shu <ys2530@nyu.edu>
2016-10-12 19:56:31 -0400

    [331. Verify Preorder Serialization of a Binary Tree][Compile Error]committed by Anthony

---
+++ 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.java
@@ -0,0 +1,52 @@
+public class Solution {
+    public boolean isValidSerialization(String preorder) {
+        // corner case
+        if (preorder == null || preorder.length() == 0) {
+            return false;
+        }
+        
+        char[] chars = preorder.toCharArray();
+        
+        Stack<Character> stack = new Stack<Character>();
+        
+        for (int i = 0; i < chars.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(chars[i]);
+            } else {
+                // if (chars[i] == '#' && !stack.isEmpty() && stack.peek() == '#') {
+                //     if (stack.isEmpty()) {
+                //         stack.pop();                        
+                //     } else {
+                //         return false;
+                //     }
+                    
+                //     if (stack.isEmpty()) {
+                //         stack.pop();
+                //     } else {
+                //         return false;
+                //     }
+                //     stack.push('#');
+                // }
+                char c = char[i];
+                check(stack, c);
+                stack.push(c);
+            }
+        }
+        
+        return !stack.isEmpty() && stack.size() == 1 && stack.peek() == "#";
+    }
+    
+    public void check(Stack<Character> stack, char c) {
+        if (c == '#' && !stack.isEmpty() && stack.peek() == '#') {
+            stack.pop();
+            if (stack.isEmpty()) {
+                return;
+            } else {
+                stack.pop();
+                check(stack, c);
+            }
+        } else {
+            return;
+        }
+    }
+}
